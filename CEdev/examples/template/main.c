65aa52b17373479c6fe1635e3cba2985fe9641af
Matt Waltz <matthewwaltzis@gmail.com>
2016-01-28 18:24:12 -0800

    Changed example program a bit

--- CEdev/examples/template/main.c
+++ CEdev/examples/template/main.c
@@ -10,49 +10,85 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Function Prototypes */
-void print(char* string, uint8_t xpos, uint8_t ypos);
+/* Other available headers */
+// stdarg.h, setjmp.h, assert.h, ctype.h, float.h, iso646.h, limits.h, errno.h
+
+
+/* Put your function prototypes here */
+
+void print(const char* string, uint8_t col, uint8_t row);
+void printSmall(const char* string, uint16_t x, uint8_t y);
 void cleanUp();
 
-const static char hello[] = "HELLO WORLD!";
-const static char welcome[] = "Welcome To The World Of C!";
-const static char Apples[] ="Apples.";
-const static char Orange[] ="Oranges.";
-    
-void main() {
-	uint8_t i;
-    
-	_OS( cleanUp();
-	     print(hello, 0, 0);
-       );
-	
-	for(i=1; i<5; i++) {
-		_OS( print(welcome, 0, i) );
-	}
-	
-	strcpy(Apples, Orange);
-    
-	_OS( print(Apples, 0, i);       /* Should print "Oranges."   */
-	     print(Orange, 0, i+1);     /* Should also print "Oranges."   */
-         GetKey();
-	     cleanUp();
+
+/* Put all your globals here. */
+
+const static char hello[]   = "HELLO WORLD!";
+const static char welcome[] = "Welcome to C programming!";
+
+
+/* Put all your code here */
+
+void main()
+{
+    // initial variables
+    char Apples[] = "Apples.";
+    char Orange[] = "Orange.";
+    uint8_t i;
+
+    cleanUp();
+
+    print(hello, 0, 0);
+
+    for (i=1; i<5; i++)
+    {
+        print(welcome, 0, i);
+    }
+
+    strcpy(Apples, Orange);
+    print(Apples, 0, i);    // Should print "Orange."
+    print(Orange, 0, i+1);  // Should also print "Orange."
+
+    _OS( GetKey() );
+    cleanUp();
+}
+
+
+
+/* Other functions */
+
+void print(const char* string, uint8_t xpos, uint8_t ypos)
+{
+    _OS( asm("LD HL,(IX+6)");
+         asm("LD A,(IX+9)");
+         asm("LD (curCol),A");
+         asm("LD A,(IX+12)");
+         asm("LD (curRow),A");
+         asm("CALL _PutS");
        );
 }
 
-void cleanUp() {
-	asm("CALL _DelRes");
-	asm("CALL _ClrTxtShd");
-	asm("CALL _ClrScrn");
-	asm("set 0,(iy+3)");
-	asm("CALL _HomeUp");
-	asm("CALL _DrawStatusBar");
+void printSmall(const char* string, uint16_t x, uint8_t y)
+{
+    _OS( asm("LD HL,(IX+6)");
+         asm("LD DE,(IX+9)");
+         asm("LD (penCol),DE");
+         asm("LD A,(IX+12)");
+         asm("LD (penRow),A");
+         asm("RES textInverse,(IY+textFlags)"); // SET for inverted
+         asm("CALL _VPutS");
+       );
 }
 
-void print(char* string, uint8_t xpos, uint8_t ypos) {
-	asm("LD A,(IX+9)");
-	asm("LD (D00596h),A");
-	asm("LD A,(IX+12)");
-	asm("LD (D00595h),A");
-	asm("LD	HL,(IX+6)");
-	asm("CALL _PutS");
+void cleanUp()
+{
+    // Clear/invalidate some RAM areas
+    _OS( asm("CALL _DelRes");
+         asm("CALL _ClrTxtShd");
+         asm("CALL _ClrScrn");
+         asm("SET  graphDraw,(iy+graphFlags)");
+         // Restore the home screen nicely
+         asm("CALL _HomeUp");
+         asm("CALL _DrawStatusBar");
+       );
 }
