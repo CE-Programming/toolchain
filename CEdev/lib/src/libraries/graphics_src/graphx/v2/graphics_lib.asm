0f89f83a714ebdce4fb1cdc7e301304ddf1c14cc
Gérald DeGroote <grosged@users.noreply.github.com>
2016-10-31 19:00:33 +0100

    Update graphics_lib.asm

--- CEdev/lib/src/libraries/graphics_src/graphx/v2/graphics_lib.asm
+++ CEdev/lib/src/libraries/graphics_src/graphx/v2/graphics_lib.asm
@@ -1864,46 +1864,50 @@ _Sprite_NoClip:
 ;  arg2 : Y Coord
 ; Returns:
 ;  None
-		ld	iy,0
-		add	iy,sp
-		ld	hl,(currDrawBuffer) \.r
-		ld	bc,(iy+6)			;  x coordinate
-		add	hl,bc
-		ld	d,lcdWidth/2
-		ld	e,(iy+9)			;  y coordinate
-		mlt	de
-		add	hl,de
-		add	hl,de
-		ex	de,hl				; de = start draw location
-		ld	iy,(iy+3)			; iy = sprite structure
-		ld	a,(iy+1)			; sprite height
-		push	af
-		ld	b,0
-		ld	c,(iy+0)			; sprite width
-		ld	hl,lcdWidth
-		sbc	hl,bc
-		rr	h
-		ld	a,l
-		rra	
-		lea	hl,iy+2				; hl = sprite data
-		ld	iyh,c				; sprite width
-		ld	iyl,a				; = (lcdWidth-SpriteWidth)/2
-		ld	a,$f3
-		sbc	a,b
-		ld	(SprNcJrStep+1),a \.r	
-SprNcSprHcnt:	pop	af				; a = sprite height
-		jr	SprNcLpStart
-		inc	de				; needed if sprite width is odd
-SprNcLpEvenW:	ld	c,iyl				; = (lcdWidth-SpriteWidth)/2
-		ex	de,hl
-		add	hl,bc
-		add	hl,bc
-		ex	de,hl
-		ld	c,iyh				; sprite width
-SprNcLpStart:	ldir
-		dec	a
-SprNcJrStep:	jr	nz,SprNcLpEvenW
-		re
+	ld	iy,0
+	add	iy,sp
+	ld	hl,(currDrawBuffer) \.r
+	ld	bc,(iy+6)			;  x coordinate
+	add	hl,bc
+	ld	d,lcdWidth/2
+	ld	e,(iy+9)			;  y coordinate
+	mlt	de
+	add	hl,de
+	add	hl,de
+	ex	de,hl				; de = start draw location
+	ld	iy,(iy+3)			; iy = sprite structure
+	ld	a,(iy+1)			; sprite height
+	push	af
+	ld	b,0
+	ld	c,(iy+0)			; sprite width
+	ld	hl,lcdWidth
+	sbc	hl,bc
+	rr	h
+	ld	a,l
+	rra	
+	lea	hl,iy+2				; hl = sprite data
+	ld	iyh,c				; sprite width
+	ld	iyl,a				; = (lcdWidth-SpriteWidth)/2
+	ld	a,$f3
+	sbc	a,b
+	ld	(SprNcJrStep+1),a \.r	
+SprNcSprHcnt:
+	pop	af				; a = sprite height
+	jr	SprNcLpStart
+	inc	de				; needed if sprite width is odd
+SprNcLpEvenW:
+	ld	c,iyl				; = (lcdWidth-SpriteWidth)/2
+	ex	de,hl
+	add	hl,bc
+	add	hl,bc
+	ex	de,hl
+	ld	c,iyh				; sprite width
+SprNcLpStart:
+	ldir
+	dec	a
+SprNcJrStep:	
+	jr	nz,SprNcLpEvenW
+	ret
 
 ;-------------------------------------------------------------------------------
 _GetSprite_NoClip:
