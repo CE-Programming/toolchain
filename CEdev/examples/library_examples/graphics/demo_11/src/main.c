99332240aea462bcbd5c1d46c9b70cbc20ec2ca5
Zachary Wassall <runer112@gmail.com>
2017-02-04 15:24:23 -0500

    Graphics demo 11 demonstrates and tests gfx_Darken and gfx_Lighten better

--- CEdev/examples/library_examples/graphics/demo_11/src/main.c
+++ CEdev/examples/library_examples/graphics/demo_11/src/main.c
@@ -13,6 +13,8 @@
 /* Shared libraries */
 #include <lib/ce/graphx.h>
 
+#define color gfx_RGBTo1555(34, 55, 89)
+
 /* Put function prototypes here */
 
 /* Put all your code here */
@@ -22,25 +24,40 @@ void main(void) {
     /* Initialize the 8bpp graphics */
     gfx_Begin( gfx_8bpp );
 	
+    /* For i in 0..255 */
     do {
-        /* Change palette entries 0 and 1 */
-        gfx_palette[0] = gfx_Darken(gfx_RGBTo1555(255,255,255), i);
-        gfx_palette[1] = gfx_Lighten(gfx_RGBTo1555(0,0,150), i);
         
-        /* Draw using palette 0 entry */
+        /* Fade out to black */
+        gfx_palette[0] = gfx_Darken(color, i);
+        /* Fade in from black */
+        gfx_palette[1] = gfx_Darken(color, ~i);
+        /* Fade in from white */
+        gfx_palette[3] = gfx_Lighten(color, ~i);
+        /* Fade out to white */
+        gfx_palette[2] = gfx_Lighten(color, i);
+        
+        /* Fade out to black */
         gfx_SetColor(0);
         gfx_FillRectangle_NoClip(0,0,160,120);
-        gfx_FillRectangle_NoClip(160,120,160,120);
-        
-        /* Draw using palette 1 entry */
+        /* Fade in from black */
         gfx_SetColor(1);
         gfx_FillRectangle_NoClip(160,0,160,120);
+        /* Fade in from white */
+        gfx_SetColor(2);
         gfx_FillRectangle_NoClip(0,120,160,120);
+        /* Fade out to white */
+        gfx_SetColor(3);
+        gfx_FillRectangle_NoClip(160,120,160,120);
+
+        /* Wait for a keypress at the start of each quarter of the fade */
+        if (!(i & 0x3f)) {
+            while(!os_GetCSC());
+        }
         
     /* Loop until i is 0 again because of 8 bit range */
     } while(++i);
 	
-    /* Wait for key */
+    /* Wait for a keypress */
     while(!os_GetCSC());
 	
     /* Usual cleanup */
