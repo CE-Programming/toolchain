0764934ebf6e4c4a82090c6f2ae5973d5ff1d748
Adrien Bertrand <bertrand.adrien@gmail.com>
2017-02-25 12:56:49 +0100

    Fixed graphx demo3 .8xp and test file.

--- CEdev/examples/library_examples/graphics/demo_3/src/main.c
+++ CEdev/examples/library_examples/graphics/demo_3/src/main.c
@@ -19,20 +19,19 @@
 void main(void) {
     uint8_t y;
     uint24_t i;
-    
+
     /* Triangle coordinates */
-    int verts[6] = { 
+    int verts[6] = {
             160,  1,  /* (x0, y0) */
             1,  238,  /* (x1, y1) */
             318,238,  /* (x2, y2) */
     };
-    
+
     /* Initialize the 8bpp graphics */
     gfx_Begin( gfx_8bpp );
 
-    /* Draw a yellow rectangle outline */
     gfx_SetColor( gfx_blue );
-    
+
     /* Draw some circles to make sure they all look the same */
     y = 40;
     for (i = 2; i < 13; i++) {
@@ -52,46 +51,46 @@ void main(void) {
         gfx_Circle(110, y, i);
         y = y + i + 3;
     }
-    
+
     /* Wait for any key */
     while(!os_GetCSC());
-    
+
     gfx_SetColor( gfx_yellow );
     gfx_Rectangle( 0, 0, 320, 240 );
-    
+
     /* Draw a blue triangle outline */
     gfx_SetColor( gfx_blue );
     gfx_Polygon( verts, 3 );
-    
+
     /* Draw a red circle outline */
     gfx_SetColor( gfx_red );
     gfx_Circle( 160, 152, 80 );
-    
+
     /* Wait for any key */
     while(!os_GetCSC());
-    
+
     /* Draw a filled yellow rectangle */
     gfx_SetColor( gfx_yellow );
     gfx_FillRectangle( 0, 0, 320, 240 );
-    
+
     /* Draw a filled blue triangle */
     gfx_SetColor( gfx_blue );
     gfx_FillTriangle( verts[0], verts[1], verts[2], verts[3], verts[4], verts[5] );
-    
+
     /* Draw a filled red circle */
     gfx_SetColor( gfx_red );
     gfx_FillCircle( 160, 152, 80 );
-    
+
     /* Wait for any key */
     while(!os_GetCSC());
-    
+
     /* Shift the screen around a bit */
     gfx_ShiftDown(20);
     gfx_ShiftDown(20);
 
     /* Wait for any key */
     while(!os_GetCSC());
-    
+
     /* Close the graphics and return to the OS */
     gfx_End();
     prgm_CleanUp();
