8b9be9b1fbc6a8426d1ad578406e27846f14b32d
Adrien Bertrand <bertrand.adrien@gmail.com>
2017-07-31 18:01:11 +0200

    Better deploy.sh
    From https://gist.github.com/neilime/769d733c52acdecea8d2548fb3aca3ff
    
    [ci skip]

--- .deploy.sh
+++ .deploy.sh
@@ -2,8 +2,10 @@
 
 set -o errexit
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-  exit 0
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo -e "Skipping doc deploy; just doing a build.\n"
+    exit 0
 fi
 
 rev=$(git rev-parse --short HEAD)
@@ -11,8 +13,8 @@ rev=$(git rev-parse --short HEAD)
 cd doxygen/html
 
 git init
-git config user.name "travis"
-git config user.email "travis"
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
 
 git remote add upstream "https://$GH_TOKEN@github.com/CE-Programming/toolchain.git"
 git fetch upstream && git reset upstream/gh-pages
