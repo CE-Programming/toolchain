839adbfde115fc6d16a9b82cfd041d29bf290788
Matt Waltz <matthewwaltzis@gmail.com>
2017-08-09 00:43:49 -0600

    Remove/modify interrupt examples

--- examples/interrupt_on/readme.md
+++
@@ -1,8 +0,0 @@
-### On Interrupt Demo
-
-This demo demonstrates how to use basic interrupts such as the [ON] key
-
----
-
-This demo is a part of the C SDK Toolchain for use on the CE.
-
--- examples/real_time_clock/src/main.c
+++ examples/real_time_clock/src/main.c
@@ -7,14 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <intce.h>
-
-/* Function prototypes */
-void interrupt isr_rtc(void);
-void interrupt isr_on(void);
-
-unsigned seconds = 0;
-bool exit_loop = false;
+unsigned int seconds = 0;
 
 void main(void) {
 
@@ -24,14 +17,6 @@ void main(void) {
     /* Randomize things */
     srand(rtc_Time());
 
-    /* Initialize the interrupt handlers */
-    int_Initialize();
-
-    /* Set the isr_on routine to run when the [ON] key is pressed */
-    int_SetVector(ON_IVECT, isr_on);
-    int_SetVector(RTC_IVECT, isr_rtc);
-    int_EnableConfig = INT_ON | INT_RTC;
-
     /* Configure the RTC */
     rtc_LoadSeconds = rtc_LoadMinutes = rtc_LoadHours = rtc_LoadDays = 0;
     rtc_Control = RTC_ENABLE | RTC_LOAD | RTC_SEC_INT_SOURCE;
@@ -40,31 +25,15 @@ void main(void) {
     while (rtc_IsBusy());
     rtc_IntAcknowledge = RTC_INT_MASK;
 
-    /* Enable interrupts */
-    int_Enable();
-
-    while (!exit_loop);
-
-    /* Reset interrupts */
-    int_Reset();
-}
-
-void interrupt isr_rtc(void) {
-    /* Get the status of the RTC interrupt register */
-    uint8_t status = rtc_IntStatus;
-
-    /* If a second passed, increment a counter */
-    if (status & RTC_SEC_INT) {
-        memset(lcd_Ram, randInt(0,255), LCD_SIZE);
-        seconds++;
+    /* Wait until 5 seconds have passed */
+    while (seconds < 5) {
+        
+        /* If a second passed, increment the second counter */
+        if (rtc_IntStatus & RTC_SEC_INT) {
+            memset(lcd_Ram, randInt(0,255), LCD_SIZE);
+            seconds++;
+            
+            rtc_IntAcknowledge = rtc_IntStatus;
+        }
     }
-
-    /* Acknowledge all outstanding interrupts */
-    rtc_IntAcknowledge = status;
 }
-
-/* This interrupt is triggered when the [ON] key is pressed */
-void interrupt isr_on(void) {
-    exit_loop = true;
-}
-
