31e7b5e6db9b6c8569e05421892cf19274bb9b95
MateoConLechuga <matthewwaltzis@gmail.com>
2017-12-28 19:00:09 -0800

    Fix free

--- src/std/shared/free.c
+++ src/std/shared/free.c
@@ -1,10 +1,10 @@
-/************************************************************************/
-/*									*/
-/*			Copyright (C) 2000-2008 Zilog, Inc.		*/
-/*									*/
-/*		        San Jose, California     			*/
-/*									*/
-/************************************************************************/
+/***************************************************************/
+/*                                                             */
+/*            Copyright (C) 2000-2008 Zilog, Inc.              */
+/*                                                             */
+/*                   San Jose, California                      */
+/*                                                             */
+/***************************************************************/
 #include <stdlib.h>
 
 /*************************************************
@@ -12,29 +12,40 @@
 * free - free up allocated memory
 *
 * Inputs:
-*	ap - pointer to allocated memory
+*    ap - pointer to allocated memory
 *
 * Returns:
-*	nothing
+*    nothing
 *
 *************************************************/
 extern HEADER _alloc_base;
 
-void free(void *ap)
-{
-	HEADER *p,*q=(HEADER*)ap - 1;
-	for(p=&_alloc_base; p->s.ptr && p->s.ptr < q; p=p->s.ptr);
-	if((char*)p->s.ptr == ((char*)q) + q->s.size) {		/* join upper */
-		q->s.size += p->s.ptr->s.size;
-		q->s.ptr = p->s.ptr->s.ptr;
-	}
-	else
-		q->s.ptr = p->s.ptr;
-	if(((char*)p) + p->s.size == (char*)q) {		/* join lower */
-		p->s.size += q->s.size;
-		p->s.ptr = q->s.ptr;
-	}
-	else
-		p->s.ptr = q;
+void free(void *ap) {
+    HEADER *p;
+    HEADER *q;
+    
+    if (!ap) {
+        return;
+    }
+    
+    q = (HEADER*)ap - 1;
+    
+    for (p = &_alloc_base; p->s.ptr && p->s.ptr < q; p = p->s.ptr);
+    
+    /* join upper */
+    if ((char*)p->s.ptr == ((char*)q) + q->s.size) {
+        q->s.size += p->s.ptr->s.size;
+        q->s.ptr = p->s.ptr->s.ptr;
+    } else {
+        q->s.ptr = p->s.ptr;
+    }
+    
+    /* join lower */
+    if (((char*)p) + p->s.size == (char*)q) {
+        p->s.size += q->s.size;
+        p->s.ptr = q->s.ptr;
+    } else {
+        p->s.ptr = q;
+    }
 }
 
