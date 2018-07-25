50e58d5c895350b5a6c828fed42c46dcb9eaa17a
Peter Tillema <petertillema@solcon.nl>
2018-07-25 14:50:06 +0200

    Add ti_DetectAny() test
    See https://github.com/CE-Programming/toolchain/pull/155 for fix

---
+++ examples/fileio_detectany/src/main.c
@@ -0,0 +1,49 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fileioc.h>
+
+/* Function prototypes */
+void printText(int8_t xpos, int8_t ypos, const char *text);
+
+/* Main Function */
+void main(void) {
+    /* Declare some variables -- search_pos must be NULL to begin with */
+    uint8_t *search_pos = NULL, type;
+    int8_t y = 0;
+    ti_var_t myVar;
+    char *var_name;
+
+    /* First couple bytes of the LibLoad AppVar, which is known to exist *
+    /* Technically is a null-terminated string, if an odd looking one */
+    const char search_string[] = { 0xFD, 0x21, 0x80, 0x00 };
+
+    /* Clear the homescreen */
+    os_ClrHome();
+
+    /* Find all of the variables that start with this string */
+    while ((var_name = ti_DetectAny(&search_pos, search_string, &type)) != NULL) {
+        if (type == TI_APPVAR_TYPE) {
+            /* Print the name of the variable (Should be LibLoad) */
+            printText(0, y++, var_name);
+        }
+    }
+
+    /* Wait for a key */
+    while (!os_GetCSC());
+
+    /* Close all open files */
+    ti_CloseAll();
+}
+
+/* Draw text on the homescreen at the given X/Y location */
+void printText(int8_t xpos, int8_t ypos, const char *text) {
+    os_SetCursorPos(ypos, xpos);
+    os_PutStrFull(text);
+}
