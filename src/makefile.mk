a4bfcecfb6e9aaa9821a52950367923f9af00c12
MateoConLechuga <matthewwaltzis@gmail.com>
2018-07-24 22:39:49 -0600

    bump toolchain version

--- src/makefile.mk
+++ src/makefile.mk
@@ -17,7 +17,7 @@ BINDIR              ?= bin
 GFXDIR              ?= src/gfx
 #----------------------------
 
-VERSION := 8.2
+VERSION := 8.3
 
 #----------------------------
 # try not to edit anything below these lines unless you know what you are doing
