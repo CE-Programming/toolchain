e9b62e579765337940afcd734bb80e6f20c33208
Jacob Young <jacobly0@users.noreply.github.com>
2018-08-31 14:04:53 -0400

    Fixed autotester on appveyor.

--- run_autotester.bat
+++ run_autotester.bat
@@ -2,7 +2,7 @@
 echo Autotester Runner
 echo ==================
 set RESULT=0
-for /d %%d in (%cd%\examples\*) do (for %%f in (%%d\*.json) do (
+for /d %%d in (%CEDEV%\examples\*) do (for %%f in (%%d\*.json) do (
 	echo Launching autotester on %%f
 	autotester "%%f"
 	set /a "RESULT=%RESULT% + %errorlevel%"
