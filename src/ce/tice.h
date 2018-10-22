eeef0eb1f2e9353091a417142e99fe46dcd5c8aa
Zachary Wassall <runer112@gmail.com>
2018-10-22 17:46:05 -0400

    Add 32-bit "atomic" load functions

--- src/ce/tice.h
+++ src/ce/tice.h
@@ -51,6 +51,62 @@ void srandom(uint32_t seed);
 void delay(uint16_t msec);
 
 /**
+ * "Atomically" loads from a volatile 32-bit value.
+ *
+ * @remarks
+ * The hardware does not provide a mechanism to truly atomically load from a
+ * 32-bit value. This "atomic" load is implemented by non-atomically reading the
+ * value twice and retrying if the values read differ.
+ *
+ * @attention
+ * If the maximum period between two value changes is 1us or less (assuming a
+ * CPU clock speed of 48MHz), then this function may never complete. For
+ * instance, the counter of a timer ticking at 1MHz or more should not be read
+ * using this function. In such a case of a purely increasing or decreasing
+ * value, atomic_load_increasing_32() or atomic_load_decreasing_32() may be
+ * appropriate instead.
+ *
+ * @param p pointer to 32-bit value
+ */
+uint32_t atomic_load_32(volatile uint32_t *p);
+
+/**
+ * "Atomically" loads from a volatile, increasing 32-bit value.
+ *
+ * @remarks
+ * The hardware does not provide a mechanism to truly atomically load from a
+ * 32-bit value. This "atomic" load is implemented by temporarily disabling
+ * interrupts while non-atomically reading the value twice and then returning
+ * the lesser of the two values read.
+ *
+ * @attention
+ * If the minimum period between two value changes is 1us or less and the
+ * value's maximum rate of change over a 1us period exceeds 256 (assuming a CPU
+ * clock speed of 48MHz), then the value returned may be incorrect.
+ *
+ * @param p pointer to 32-bit value
+ */
+uint32_t atomic_load_increasing_32(volatile uint32_t *p);
+
+/**
+ * "Atomically" loads from a volatile, decreasing 32-bit value.
+ *
+ * @remarks
+ * The hardware does not provide a mechanism to truly atomically load from a
+ * 32-bit value. This "atomic" load is implemented by temporarily disabling
+ * interrupts while non-atomically reading the value twice and then returning
+ * the greater of the two values read.
+ *
+ * @attention
+ * If the minimum period between two value changes is 1us or less and the
+ * value's maximum rate of change over a 1us period exceeds 256 (assuming a CPU
+ * clock speed of 48MHz), then the value returned may be incorrect.
+ *
+ * @param p pointer to 32-bit value
+ */
+uint32_t atomic_load_decreasing_32(volatile uint32_t *p);
+
+/**
  * Gets a combination of the RTC time; useful for srand()
  */
 #define rtc_Time()              (*(volatile uint32_t*)0xF30044)
