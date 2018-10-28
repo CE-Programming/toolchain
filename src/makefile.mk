87868e1e1e0154c051915fd414781959f0a38299
mateoconlechuga <matthewwaltzis@gmail.com>
2018-10-28 09:36:23 -0600

    update version

--- src/makefile.mk
+++ src/makefile.mk
@@ -17,7 +17,7 @@ BINDIR              ?= bin
 GFXDIR              ?= src/gfx
 #----------------------------
 
-VERSION := 8.4
+VERSION := 8.5
 
 #----------------------------
 # try not to edit anything below these lines unless you know what you are doing
