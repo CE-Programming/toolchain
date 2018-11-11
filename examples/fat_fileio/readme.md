e97c2c6d2efbee9cd2568d95fac320f092a25b75
mateoconlechuga <matthewwaltzis@gmail.com>
2018-11-10 18:42:43 -0700

    fix includes

---
+++ examples/fat_fileio/readme.md
@@ -0,0 +1,10 @@
+### FAT File Demo
+
+This demo shows using the FAT library to read and write files present on the device, and copy them to the calculator.
+
+![Screenshot](screenshot.png)
+
+---
+
+This demo is a part of the C SDK Toolchain for use on the CE.
+
---
+++ examples/fat_fileio/src/main.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <usbfatce.h>
+
+/* Function prototypes */
+void printText(int8_t xpos, int8_t ypos, const char *text);
+void init_msd(void);
+
+/* Main Function */
+void main(void) {
+    os_ClrHome();
+
+    init_msd();
+
+    while (!os_GetCSC());
+}
+
+void init_msd(void) {
+
+    os_line("insert msd...");
+
+    /* initialize mass storage device */
+    if (!msd_Init()) {
+        os_line("msd init failed.");
+        return;
+    }
+
+    os_line("locating filesystem");
+}
+
+/* Draw text on the homescreen at the given X/Y location */
+void printText(int8_t xpos, int8_t ypos, const char *text) {
+    os_SetCursorPos(ypos, xpos);
+    os_PutStrFull(text);
+}
