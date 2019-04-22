08460a143b499c2f2ca15b99234329687492e8d7
Ander D'nar <drdnar@gmail.com>
2019-04-22 18:48:05 -0400

    Update font pack example to be fully usable

--- examples/fontlib_font_pack/src/main.c
+++ examples/fontlib_font_pack/src/main.c
@@ -13,8 +13,8 @@
 
 /* Main Function */
 void main(void) {
-    char *title = " FONTLIBC ";
-    char *font_pack_name = "HELVE";
+    char *test_str = "Hello, world! This is a much longer test string!\n";
+    char *font_pack_name = "DRSANS";
     fontlib_font_t *font_pack;
     gfx_Begin();
     /* Erase the screen to black */
@@ -24,15 +24,32 @@ void main(void) {
     fontlib_SetColors(0xC0, 0x20);
     fontlib_SetTransparency(true);
     fontlib_SetNewlineOptions(FONTLIB_ENABLE_AUTO_WRAP);
+    gfx_SetTextFGColor(0xC0);
+    gfx_SetTextBGColor(0x20);
     
     /* Set a font to use. */
-    font_pack = fontlib_GetFontByIndex(font_pack_name, 4);
-    
+    font_pack = fontlib_GetFontByIndex(font_pack_name, 1);
     if (font_pack) {
         fontlib_SetFont(font_pack, 0);
-        fontlib_DrawString("Hello, world! A much longer test string!");
-    }
+        fontlib_DrawString(test_str);
+    } else
+        gfx_PrintStringXY("1 not found", 0, 200);
     
+    /* Try getting another font */
+    font_pack = fontlib_GetFontByStyle(font_pack_name, 9, 10, 0x80, 0x80, 0, FONTLIB_SERIF);
+    if (font_pack) {
+        fontlib_SetFont(font_pack, 0);
+        fontlib_DrawString(test_str);
+    } else
+        gfx_PrintStringXY("2 not found", 100, 200);
+    
+    /* Try getting a non-existent font */
+    font_pack = fontlib_GetFontByStyle(font_pack_name, 30, 50, 0x80, 0x80, FONTLIB_SERIF, 0);
+    if (font_pack) {
+        fontlib_SetFont(font_pack, 0);
+        fontlib_DrawString(test_str);
+    } else
+        gfx_PrintStringXY("3 not found", 200, 200);
 
     /* Pause */
     while (!os_GetCSC());
