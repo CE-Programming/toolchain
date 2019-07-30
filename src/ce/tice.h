bdcf66524a7e4bf6b983097ba6a3f2c87ca2373f
Jacob Young <jacobly0@users.noreply.github.com>
2019-07-30 01:28:13 -0400

    Add new msd os functions.

--- src/ce/tice.h
+++ src/ce/tice.h
@@ -855,6 +855,62 @@ typedef uint8_t sk_key_t;
 sk_key_t os_GetCSC(void);
 
 /**
+ * Perform an MSD GetMaxLUN request, either through the USB port or with an ARM coprocessor.
+ * @param value On input the wValue to use in the request (should be zero for usb?), on output the max logical unit number.
+ */
+void os_MSDGetMaxLUN(uint8_t *value);
+
+/**
+ * Perform an MSD mass storage reset request, either through the USB port or with an ARM coprocessor.
+ * @param value Affects the wValue of the request, 1 for 0x55AA, 2 for 0xAA55, otherwise 0x0000 (for usb).
+ */
+void os_MSDReset(uint8_t value);
+
+/**
+ * Performs an MSD inquiry, either through the USB port or with an ARM coprocessor.
+ * @param lun Logical unit number.
+ * @param status Returns the status of the command.
+ */
+void os_MSDInquiry(uint8_t lun, uint8_t *status);
+
+/**
+ * Performs an MSD test unit ready command, either through the USB port or with an ARM coprocessor.
+ * @param lun Logical unit number.
+ * @param status Returns the status of the command.
+ */
+void os_MSDTestUnitReady(uint8_t lun, uint8_t *status);
+
+/**
+ * Performs an MSD read capacity command, either through the USB port or with an ARM coprocessor.
+ * @param lun Logical unit number.
+ * @param data Returns the returned logical block address and block byte length.
+ * @return Error code, 0 for success, 5 for failed command, -5 for failed transfer.
+ */
+int8_t os_MSDReadCapacity(uint8_t lun, uint32_t data[2]);
+
+/**
+ * Performs an MSD read command, either through the USB port or with an ARM coprocessor.
+ * @param lun Logical unit number.
+ * @param blockCount Number of blocks to read.
+ * @param lba Logical block address to start reading from.
+ * @param blockSize Block size, obtained from os_MSDReadCapacity().
+ * @param buffer Buffer to read data into, should be blockCount * blockSize bytes.
+ * @return Error code, 0 for success, 5 for failed command, -5 for failed transfer.
+ */
+int8_t os_MSDRead(uint8_t lun, uint8_t blockCount, uint32_t lba, uint24_t blockSize, void *buffer);
+
+/**
+ * Performs an MSD write command, either through the USB port or with an ARM coprocessor.
+ * @param lun Logical unit number.
+ * @param blockCount Number of blocks to write.
+ * @param lba Logical block address to start writing to.
+ * @param blockSize Block size, obtained from os_MSDReadCapacity().
+ * @param buffer Buffer of data to write, should be blockCount * blockSize bytes.
+ * @return Error code, 0 for success, 5 for failed command, -5 for failed transfer.
+ */
+int8_t os_MSDWrite(uint8_t lun, uint8_t blockCount, uint32_t lba, uint24_t blockSize, void *buffer);
+
+/**
  * Runs the calulator at 6 MHz
  */
 void boot_Set6MHzMode(void);
