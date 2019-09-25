f87a7d7ddfb37b6c1a1f6092f5792a296a6c3630
Jacob Young <jacobly0@users.noreply.github.com>
2019-09-25 20:53:27 +0200

    Move doc example to real example.

--- src/usbdrvce/usbdrvce.h
+++ src/usbdrvce/usbdrvce.h
@@ -520,57 +520,8 @@ typedef usb_error_t (*usb_transfer_callback_t)(usb_endpoint_t endpoint,
  * This struct represents a timed callback.  It must be allocated by the user.
  * The only public member is handler, which must be initialized before use.
  * If you want to access other data from the callback, allocate a larger
- * struct with this struct as its first member and cast like in this example:
- * \code
- * #include <tice.h>
- * #include <usbdrvce.h>
- *
- * typedef struct my_timer {
- *     usb_timer_t usbTimer;
- *     unsigned interval;
- *     unsigned counter;
- * } my_timer_t;
- *
- * usb_error_t my_timer_handler(usb_timer_t *usbTimer) {
- *     my_timer_t *myTimer = (my_timer_t *)usbTimer;
- *     if (myTimer->counter != 0) {
- *         myTimer->counter -= 1;
- *     }
- *     usb_RepeatTimer(&myTimer, myTimer->interval);
- *     return USB_SUCCESS;
- * }
- *
- * #define N 4
- *
- * void main() {
- *     os_ClrHomeFull();
- *     if (usb_Init(NULL, NULL, NULL, USB_DEFAULT_INIT_FLAGS) == USB_SUCCESS) {
- *         my_timer_t myTimers[N];
- *         int i;
- *         for (i = 0; i != N; i++) {
- *             myTimers[i].usbTimer.handler = my_timer_handler;
- *             myTimers[i].interval = 1000u >> i;
- *             myTimers[i].counter = 10u << i;
- *             usb_StartTimer(&myTimers[i], myTimers[i].interval);
- *         }
- *         while (usb_WaitForEvents() == USB_SUCCESS) {
- *             for (i = 0; i != N; i++) {
- *                 char string[3];
- *                 os_SetCursorPos(i, 0);
- *                 sprintf(string, "%2u", myTimers[i].counter);
- *                 os_PutStringFull(string);
- *             }
- *             for (i = 0; i != N; i++) {
- *                 if (myTimers[i].counter != 0) {
- *                     continue;
- *                 }
- *             }
- *             break;
- *         }
- *         usb_Cleanup();
- *     }
- * }
- * \endcode
+ * struct with this struct as its first member and cast like in the usb_timer
+ * example.
  */
 typedef struct usb_timer usb_timer_t;
 
