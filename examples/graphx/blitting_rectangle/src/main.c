1835683c6ebb80d62996aa00e2950f03376fdd4c
mateoconlechuga <matthewwaltzis@gmail.com>
2020-01-11 14:07:34 -0700

    add more autotests

--- examples/graphx/blitting_rectangle/src/main.c
+++ examples/graphx/blitting_rectangle/src/main.c
@@ -3,36 +3,50 @@
 
 int main(void)
 {
+    uint8_t c;
+
     /* Initialize graphics drawing */
     gfx_Begin();
 
-    /* Fill the screen */
-    gfx_FillScreen(0);
-
-    /* Draw to the offscreen buffer */
+    /* Draw to buffer instead of directly to screen */
+    /* This is the same as gfx_SetDraw(gfx_buffer) */
     gfx_SetDrawBuffer();
 
-    /* Fill the buffer */
-    gfx_FillScreen(255);
+    /* Draw a gradient (to the buffer) */
+    for (c = 0; c < 8; c++)
+    {
+        gfx_SetColor(c);
+        gfx_FillRectangle_NoClip(c * (LCD_WIDTH / 8), 0, LCD_WIDTH / 8, LCD_HEIGHT);
+    }
 
-    /* Draw a rectangle outline */
-    gfx_SetColor(224);
-    gfx_Rectangle(30, 20, 40, 50);
-
-    /* Copy the drawn rectangle */
-    gfx_CopyRectangle(gfx_buffer, gfx_buffer, 30, 20, 80, 20, 40, 50);
-
-    /* Copy the drawn rectangles to the visible screen */
-    gfx_CopyRectangle(gfx_buffer, gfx_screen, 30, 20, 30, 20, 90, 50);
-
-    /* Copy the drawn rectangles on the visible screen to the buffer */
-    gfx_CopyRectangle(gfx_screen, gfx_buffer, 30, 20, 30, 110, 90, 50);
-
-    /* Wait for key input */
+    /* Waits for a key */
     while (!os_GetCSC());
 
-    /* Copy the whole buffer to the visible screen */
-    gfx_BlitBuffer();
+    /* Copy part of the drawn gradient to the visible screen */
+    gfx_CopyRectangle(gfx_buffer, gfx_screen, LCD_WIDTH / 4, LCD_HEIGHT / 4, LCD_WIDTH / 4, LCD_HEIGHT / 4, LCD_WIDTH / 2, LCD_HEIGHT / 2);
+
+    /* Waits for a key */
+    while (!os_GetCSC());
+
+    /* This is the same as gfx_SetDraw(gfx_screen) */
+    gfx_SetDrawScreen();
+
+    /* Draw a gradient (to the screen) */
+    for (c = 16; c < 24; c++)
+    {
+        gfx_SetColor(c);
+        gfx_FillRectangle_NoClip((c - 16) * (LCD_WIDTH / 8), 0, LCD_WIDTH / 8, LCD_HEIGHT);
+    }
+
+    /* Copy part of the drawn gradient to the offscreen buffer */
+    gfx_CopyRectangle(gfx_screen, gfx_buffer, LCD_WIDTH / 4, LCD_HEIGHT / 4, LCD_WIDTH / 4, LCD_HEIGHT / 4, LCD_WIDTH / 2, LCD_HEIGHT / 2);
+
+    /* Waits for a key */
+    while (!os_GetCSC());
+
+    /* Copy the offscreen buffer to the visible screen */
+    /* Same as gfx_BlitBuffer() */
+    gfx_CopyRectangle(gfx_buffer, gfx_screen, 0, 0, 0, 0, LCD_WIDTH, LCD_HEIGHT);
 
     /* Waits for a key */
     while (!os_GetCSC());
