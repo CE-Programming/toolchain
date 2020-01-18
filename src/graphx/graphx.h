9ed1fbbdad65d023daf43ca97f0b720f0d004be0
mateoconlechuga <matthewwaltzis@gmail.com>
2020-01-17 21:43:25 -0700

    remove graphx compatibility defines

--- src/graphx/graphx.h
+++ src/graphx/graphx.h
@@ -1633,32 +1633,15 @@ gfx_ConvertToNewRLETSprite(sprite_in, malloc)
 ((uint8_t)(b) >> 3))
 
 /**
- * Checks if we are currently in a rectangular hotspot area
+ * Checks if we are currently in a rectangular hotspot area.
+ * This function returns "true" when the test_* parameters intersect into the master_* region,
+ * otherwise false. It can be used for collision detection between rectangles.
  */
 #define gfx_CheckRectangleHotspot(master_x, master_y, master_width, master_height, test_x, test_y, test_width, test_height) \
 (((test_x) < ((master_x) + (master_width))) && \
 (((test_x) + (test_width)) > (master_x)) && \
 ((test_y) < ((master_y) + (master_height))) && \
 (((test_y) + (test_height)) > (master_y)))
-
-/* Old Color definitions (avoid using in new programs!) */
-#define gfx_black       0x00
-#define gfx_red         0xE0
-#define gfx_orange      0xE3
-#define gfx_green       0x03
-#define gfx_blue        0x10
-#define gfx_purple      0x50
-#define gfx_yellow      0xE7
-#define gfx_pink        0xF0
-#define gfx_white       0xFF
-
-/* Compatability defines (don't use please) */
-typedef gfx_sprite_t gfx_image_t;
-#define gfx_BlitArea gfx_BlitRectangle
-void gfx_LZDecompress(uint8_t *in, uint8_t *out, unsigned int in_size);
-
-#define gfx_lcdWidth    (320)
-#define gfx_lcdHeight   (240)
 
 #ifdef __cplusplus
 }
