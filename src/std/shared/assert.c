7e31074645873537e581f138f8a1c99580238b43
mateoconlechuga <matthewwaltzis@gmail.com>
2020-01-17 20:05:41 -0700

    fix assert warning

--- src/std/shared/assert.c
+++ src/std/shared/assert.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void __assert_fail_loc(const struct __assert_loc *loc) {
-    dbg_sprintf(dbgerr, "Assertion failure: %s:%u:%s:%s\n",
+    dbg_sprintf((char *)dbgerr, "Assertion failure: %s:%u:%s:%s\n",
                 loc->__file, loc->__line,
                 loc->__function, loc->__assertion);
     abort();
