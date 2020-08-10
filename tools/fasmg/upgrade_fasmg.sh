a4ea13a8641b64223a452704c042361c74c5641a
mateoconlechuga <matthewwaltzis@gmail.com>
2020-08-09 18:39:48 -0600

    use 64-bit fasmg

--- tools/fasmg/upgrade_fasmg.sh
+++ tools/fasmg/upgrade_fasmg.sh
@@ -3,7 +3,7 @@
 # this script currently only runs on linux.
 
 build_fasmg () {
-    .tmp/fasmg/fasmg .tmp/fasmg/source/$1/$3/fasmg.asm $1/$2
+    .tmp/fasmg/fasmg.x64 .tmp/fasmg/source/$1/$3/fasmg.asm $1/$2
     chmod -x $1/$2
 }
 
@@ -13,11 +13,11 @@ cd ..
 mkdir -p .tmp
 wget --no-verbose https://flatassembler.net/fasmg.zip --output-document=.tmp/fasmg.zip
 unzip -od .tmp/fasmg .tmp/fasmg.zip
-chmod +x .tmp/fasmg/fasmg
+chmod +x .tmp/fasmg/fasmg.x64
 
-build_fasmg linux   fasmg
+build_fasmg linux   fasmg      x64
 build_fasmg windows fasmg.exe
-build_fasmg macos   fasmg     x64
+build_fasmg macos   fasmg      x64
 
 rm -rf .tmp
 
