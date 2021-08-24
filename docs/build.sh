7264f9f0fd4fd7baaa8fe67ac7103740996c55a8
mateoconlechuga <matthewwaltzis@gmail.com>
2021-08-23 22:53:12 -0600

    fix the previous commit again

--- docs/build.sh
+++ docs/build.sh
@@ -3,25 +3,28 @@
 build_documentation () {
    export current_version
    git checkout ${current_version}
-   cp -f /tmp/conf.py.master conf.py
-   mkdir -p templates
-   cp -f /tmp/versions.html templates
+   cp -f /tmp/conf.py conf.py
 
    echo "Building documentation for ${current_version}..."
 
-   if [ ! -e 'conf.py' ]; then
-      continue
+   if [ -f 'templates/versions.html' ]; then
+     mkdir -p templates
+     cp -f /tmp/versions.html templates/versions.html
+     sphinx-build . build/html/${current_version}
+     rm -rf templates
+   else
+     mkdir -p templates
+     cp -f /tmp/versions.html templates/versions.html
+     sphinx-build . build/html/${current_version}
+     git checkout -- templates/versions.html
    fi
-
-   sphinx-build . build/html/${current_version}
-
-   git checkout -- conf.py templates/
+   git checkout -- conf.py
 }
 
 current_version="v9.1"
 export versions="master v9.1 v9.0"
 
-cp -f conf.py /tmp/conf.py.master
+cp -f conf.py /tmp/conf.py
 cp -f templates/versions.html /tmp/versions.html
 
 build_documentation
