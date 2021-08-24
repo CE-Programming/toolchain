0887b85c6f9cd179aa2d578a51675de1761b05ca
mateoconlechuga <matthewwaltzis@gmail.com>
2021-08-23 22:28:57 -0600

    don't clean on docs build

--- docs/build.sh
+++ docs/build.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-read -t 20 -p "Warning!! This will force-checkout branches! Press ENTER to cancel now."
-
 current_version="v9.1"
 versions="master v9.1 v9.0"
 
 export versions
 export current_version
 
-git checkout -f ${current_version}
-git clean -fxd
-
+git checkout ${current_version}
 sphinx-build . build/html
 
 for current_version in ${versions}; do
  
    export current_version
-   git checkout -f ${current_version}
-   git clean -fxd
+   git checkout ${current_version}
 
    echo "Building documentation for ${current_version}..."
 
