f67b83c9bf9b4b80d1aabf5239421d5fd3dc233f
mateoconlechuga <matthewwaltzis@gmail.com>
2021-08-26 21:31:57 -0600

    added runprgm autotest

--- examples/standalone/runprgm/src/main.c
+++ examples/standalone/runprgm/src/main.c
@@ -2,50 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void run_prgm(const char *msg, size_t msg_size, os_runprgm_callback_t callback)
+int prgm2_ret(void *data, int retval)
+{
+    printf("executed prgm 2.\n");
+    printf("retval: %d", retval);
+    (void)data;
+
+    while (!os_GetCSC());
+
+    return 0;
+}
+
+int prgm1_ret(void *data, int retval)
 {
     int ret;
 
-    /* attempt to run the program PRGM */
-    ret = os_RunPrgm("\x05PRGM", (void*)msg, msg_size, callback);
-    switch (ret)
-    {
-        case -1:
-            os_PutStrFull("PRGM not found.");
-            break;
-
-        case -2:
-            os_PutStrFull("Not enough mem.");
-            break;
-
-        default:
-            break;
-    }
-
-    while (!os_GetCSC());
-}
-
-int prgm_ret(void *data, int retval)
-{
-    /* this should print "hello world" because the string context was saved */
-    printf("%s\n", (char*)data);
+    printf("executed prgm 1.\n");
+    printf("msg: %s\n", (char*)data);
     printf("retval: %d", retval);
 
     while (!os_GetCSC());
 
-    /* run the program PRGM again, but don't use a callback this time */
-    run_prgm(NULL, 0, NULL);
+    ret = os_RunPrgm("\x05PRGM2", NULL, 0, prgm2_ret);
+    printf("Error running prgm 2: %d\n", ret);
 
     return 0;
 }
 
 int main(void)
 {
-    static const char string[] = "hello world";
+    static const char msg[] = "hello";
+    int ret;
 
-    os_ClrHome();
-
-    run_prgm(string, sizeof string, prgm_ret);
+    ret = os_RunPrgm("\x05PRGM1", (void*)msg, sizeof msg, prgm1_ret);
+    printf("Error running prgm 1: %d\n", ret);
 
     return 0;
 }
