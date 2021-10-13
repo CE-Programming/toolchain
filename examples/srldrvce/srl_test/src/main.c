098666498c208751320f3a7862349fd692956053
commandblockguy <bluestemjc@gmail.com>
2021-10-12 23:02:07 -0500

    Improve srldrvce test program

--- examples/srldrvce/srl_test/src/main.c
+++ examples/srldrvce/srl_test/src/main.c
@@ -47,8 +47,6 @@ void print_srl_dev(const srl_device_t *srl) {
 
 srl_device_t srl;
 
-usb_device_t device = NULL;
-
 bool has_srl_device = false;
 
 uint8_t srl_buf[512];
@@ -61,18 +59,44 @@ static usb_error_t handle_usb_event(usb_event_t event, void *event_data,
         printf("device connected\n");
         usb_ResetDevice(device);
     }
-    if(event == USB_HOST_CONFIGURE_EVENT) {
-        usb_device_t host = usb_FindDevice(NULL, NULL, USB_SKIP_HUBS);
-        if(host) device = host;
+
+    /* Call srl_Open on newly enabled device, if there is not currently a serial device in use */
+    if(event == USB_HOST_CONFIGURE_EVENT || (event == USB_DEVICE_ENABLED_EVENT && !(usb_GetRole() & USB_ROLE_DEVICE))) {
+
+        /* If we already have a serial device, ignore the new one */
+        if(has_srl_device) return USB_SUCCESS;
+
+        usb_device_t device;
+        if(event == USB_HOST_CONFIGURE_EVENT) {
+            /* Use the device representing the USB host. */
+            device = usb_FindDevice(NULL, NULL, USB_SKIP_HUBS);
+            if(device == NULL) return USB_SUCCESS;
+        } else {
+            /* Use the newly enabled device */
+            device = event_data;
+        }
+
+        /* Initialize the serial library with the newly attached device */
+        srl_error_t error = srl_Open(&srl, device, srl_buf, sizeof srl_buf, SRL_INTERFACE_ANY, 9600);
+        if(error) {
+            /* Print the error code to the homescreen */
+            printf("Error %u initting serial\n", error);
+            return USB_SUCCESS;
+        }
+
+        printf("serial initialized\n");
+
+        has_srl_device = true;
+        // print_srl_dev(&srl);
     }
-    /* When a device is connected, or when connected to a computer */
-    if((event == USB_DEVICE_ENABLED_EVENT && !(usb_GetRole() & USB_ROLE_DEVICE))) {
-        device = event_data;
-    }
+
     if(event == USB_DEVICE_DISCONNECTED_EVENT) {
-        srl_Close(&srl);
-        has_srl_device = false;
-        device = NULL;
+        usb_device_t device = event_data;
+        if(device == srl.dev) {
+            printf("Device disconnected\n");
+            srl_Close(&srl);
+            has_srl_device = false;
+        }
     }
 
     return USB_SUCCESS;
@@ -82,34 +106,18 @@ int main(void) {
     os_ClrHome();
     const usb_standard_descriptors_t *desc = srl_GetCDCStandardDescriptors();
     /* Initialize the USB driver with our event handler and the serial device descriptors */
-    //usb_error_t usb_error = 
-    usb_Init(handle_usb_event, NULL, desc, USB_DEFAULT_INIT_FLAGS);
-    //if(usb_error) {
-    //    usb_Cleanup();
-    //    printf("usb init error %u\n", usb_error);
-    //    do kb_Scan(); while(!kb_IsDown(kb_KeyClear));
-    //    return 1;
-    //}
+    usb_error_t usb_error = usb_Init(handle_usb_event, NULL, desc, USB_DEFAULT_INIT_FLAGS);
+    if(usb_error) {
+       usb_Cleanup();
+       printf("usb init error %u\n", usb_error);
+       do kb_Scan(); while(!kb_IsDown(kb_KeyClear));
+       return 1;
+    }
 
     do {
         kb_Scan();
         
         usb_HandleEvents();
-
-        if(device && !has_srl_device) {
-            //printf("device enabled\n");
-
-            /* Initialize the serial library with the newly attached device */
-            srl_error_t error = srl_Open(&srl, device, srl_buf, sizeof srl_buf, SRL_INTERFACE_ANY, 9600);
-
-            if(error) {
-                /* Print the error code to the homescreen */
-                printf("Error %u initting serial\n", error);
-            }
-            has_srl_device = true;
-
-            print_srl_dev(&srl);
-        }
 
         if(has_srl_device) {
             char in_buf[64];
@@ -140,11 +148,11 @@ int main(void) {
                 while(kb_IsDown(kb_KeySin)) kb_Scan();
             }
 
-            //size_t bytes_read = srl_Read(&srl, in_buf, sizeof in_buf);
-//
-            //if(bytes_read) {
-            //    //srl_Write(&srl, in_buf, bytes_read);
-            //}
+            size_t bytes_read = srl_Read(&srl, in_buf, sizeof in_buf);
+
+            if(bytes_read) {
+               srl_Write(&srl, in_buf, bytes_read);
+            }
         }
 
     } while(!kb_IsDown(kb_KeyClear));
