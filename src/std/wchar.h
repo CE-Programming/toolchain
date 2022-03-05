95ba1ac9c5227860cbdfa7a563eda239a90d3e3f
Jacob Young <jacobly0@users.noreply.github.com>
2022-03-04 22:05:52 -0500

    Fix various standard defines.

--- src/std/wchar.h
+++ src/std/wchar.h
@@ -8,9 +8,13 @@ typedef __WCHAR_TYPE__ wchar_t;
 #endif /* __cplusplus */
 #endif /* _WCHAR_T_DEFINED */
 
-#define WCHAR_MIN ~__WCHAR_MAX__
-#define WCHAR_MAX  __WCHAR_MAX__
-#define WEOF -1
+#ifndef _WCHAR_LIMITS_DEFINED
+#define _WCHAR_LIMITS_DEFINED
+#define WCHAR_WIDTH __WCHAR_WIDTH__
+#define WCHAR_MIN (~__WCHAR_MAX__)
+#define WCHAR_MAX   __WCHAR_MAX__
+#endif
 
+#define WEOF -1
 
 #endif /* _WCHAR_H */
