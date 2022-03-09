03efe3b8bffc4d5469fc12051d87d89b2095f4e4
Peter Tillema <petertillema@solcon.nl>
2022-03-09 13:41:43 +0100

    Clearify wording of x/y coordinates

--- src/graphx/graphx.h
+++ src/graphx/graphx.h
@@ -762,8 +762,8 @@ gfx_Circle((x), (y), (radius))
  *
  * This is measured from the top left origin of the screen.
  * Performs faster than using gfx_FillEllipse, but can cause corruption if used outside the bounds of the screen.
- * @param x X coordinate.
- * @param y Y coordinate.
+ * @param x X coordinate of the center.
+ * @param y Y coordinate of the center.
  * @param a The horizontal radius of the ellipse.
  * @param b The vertical radius of the ellipse.
  */
@@ -773,8 +773,8 @@ void gfx_FillEllipse_NoClip(int x, int y, int a, int b);
  * Draws a filled ellipse.
  *
  * This is measured from the top left origin of the screen.
- * @param x X coordinate.
- * @param y Y coordinate.
+ * @param x X coordinate of the center.
+ * @param y Y coordinate of the center.
  * @param a The horizontal radius of the ellipse.
  * @param b The vertical radius of the ellipse.
  */
@@ -785,8 +785,8 @@ void gfx_FillEllipse(int x, int y, int a, int b);
  *
  * This is measured from the top left origin of the screen.
  * Performs faster than using gfx_FillEllipse, but can cause corruption if used outside the bounds of the screen.
- * @param x X coordinate.
- * @param y Y coordinate.
+ * @param x X coordinate of the center.
+ * @param y Y coordinate of the center.
  * @param a The horizontal radius of the ellipse.
  * @param b The vertical radius of the ellipse.
  */
@@ -796,8 +796,8 @@ void gfx_Ellipse_NoClip(int x, int y, int a, int b);
  * Draws an ellipse.
  *
  * This is measured from the top left origin of the screen.
- * @param x X coordinate.
- * @param y Y coordinate.
+ * @param x X coordinate of the center.
+ * @param y Y coordinate of the center.
  * @param a The horizontal radius of the ellipse.
  * @param b The vertical radius of the ellipse.
  */
