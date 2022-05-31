6c1b4212428b5ce3e33607815717a1e9b44acfc0
Jacob Young <jacobly0@users.noreply.github.com>
2022-05-31 09:56:46 -0400

    Be pedantic.

--- src/linker_script.sh
+++ src/linker_script.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 while IFS= read -r line; do
     set -- $line
-    if [ $# -eq 4 ] && [ "$1x$4" = @x@ ]; then
-        find "$3" \! -name crt0.src -name \*.src -printf "`printf %s "$line" | cut -d@ -f1`$2 \"../lib/$3/%f\"\n" | sort
+    if [ $# -eq 4 ] && [ $1x$4 = @x@ ]; then
+        indent=`printf %s "$line" | cut -d@ -f1`
+        find $3 \! -iname crt\* -iname \*.src | LC_ALL=C sort | while IFS= read -r file; do
+            printf %s\\n "$indent$2 \"../lib/$3/`basename "$file"`\""
+        done
     else
-        echo "$line"
+        printf %s\\n "$line"
     fi
 done < "$1"
