aa21dd4a7d16af83b2f96019c846c4fdad6f6cf7
mateoconlechuga <matthewwaltzis@gmail.com>
2022-06-01 18:47:43 -0600

    add zx0 and clean up zx7 decompression
    Signed-off-by: mateoconlechuga <matthewwaltzis@gmail.com>

--- src/ce/include/compression.h
+++ src/ce/include/compression.h
@@ -1,6 +1,7 @@
 /**
  * @file
  * @author Matt "MateoConLechuga" Waltz
+ * @author Einar Saukas
  * @brief Optimized (de)compression routines
  */
 
@@ -12,12 +13,20 @@ extern "C" {
 #endif
 
 /**
- * Decompress a block of zx7 encoded data
+ * Decompress a block of ZX7 encoded data.
  *
- * @param[in] dst Pointer to destination for uncompressed data
- * @param[in] src Pointer to compressed data
+ * @param[in] dst Uncompressed data destination.
+ * @param[in] src Compressed data source.
 */
 void zx7_Decompress(void *dst, const void *src);
+
+/**
+ * Decompress a block of ZX0 encoded data.
+ *
+ * @param[in] dst Uncompressed data destination.
+ * @param[in] src Compressed data source.
+*/
+void zx0_Decompress(void *dst, const void *src);
 
 #ifdef __cplusplus
 }
