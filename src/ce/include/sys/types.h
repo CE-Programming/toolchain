7ef1ea4162f1b691ba74bc267752984a98f5509e
Zachary Wassall <runer112@gmail.com>
2023-09-16 23:06:14 -0400

    Add EASTL
    At least some of it has been verified to work in an actual program.

---
+++ src/ce/include/sys/types.h
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * @authors
+ * Zachary "Runer112" Wassall
+ * @brief Data types
+ */
+
+#ifndef SYS_TYPES_H
+#define SYS_TYPES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <cstddef>
+
+
+typedef ptrdiff_t ssize_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SYS_TYPES_H
