d8caae34d2267461785406284fd6cc13c1918176
Matthew Waltz <matthewwaltzis@gmail.com>
2023-11-16 21:41:28 -0700

    add regression test for float multiply bug

---
+++ test/regression/bug0002/src/main.c
@@ -0,0 +1,19 @@
+#include <ti/screen.h>
+#include <ti/getkey.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+int main(void)
+{
+    volatile uint64_t x = 3750000;
+
+    os_ClrHome();
+
+    x *= 2.5;
+
+    printf("value: %llu\n", x);
+
+    os_GetKey();
+
+    return 0;
+}
