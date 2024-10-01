009bebf35bdf554d8dd31523c5e3e9a774c297ef
Brendan Fletcher <calc84maniac@gmail.com>
2024-10-01 16:02:44 -0400

    Optimize 48-bit count leading zeros, improve regression test

--- test/regression/i48routines/src/main.c
+++ test/regression/i48routines/src/main.c
@@ -250,6 +250,11 @@ void gen_regs_signed_near_int_max(regs_t *regs) {
     regs->dehl.s_ext = (int64_t)INT24_MAX + (regs->dehl.s_ext % 2);
 }
 
+void gen_regs_unsigned_leading_zeros(regs_t *regs) {
+    gen_regs_unsigned(regs);
+    regs->dehl.z_ext >>= regs->iybc.z_ext % 49;
+}
+
 void print_regs(regs_t *regs) {
     printf("A=%02X DE=%06X HL=%06X\n"
            "F=%02X IY=%06X BC=%06X\n",
@@ -323,7 +328,7 @@ int main(void)
         TEST(signed, i48cmpzero, 0xC0)
         TEST(signed_near_zero, i48cmpzero, 0xC0)
         TEST(signed_near_int_max, i48cmpzero, 0xC0)
-        TEST(unsigned, i48ctlz, 0)
+        TEST(unsigned_leading_zeros, i48ctlz, 0)
         TEST(signed, i48divs, 0)
         TEST(unsigned, i48divu, 0)
         TEST(unsigned, i48dvrmu, 0)
