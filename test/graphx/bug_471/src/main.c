5e35560074b324ce739fc29e7d319973c8621fc2
Matthew Waltz <matthewwaltzis@gmail.com>
2024-10-06 22:31:44 -0600

    [graphx] fix and add tests for graphx #471

---
+++ test/graphx/bug_471/src/main.c
@@ -0,0 +1,19 @@
+#include <ti/getcsc.h>
+#include <graphx.h>
+
+int main(void)
+{
+    gfx_Begin();
+
+    gfx_SetColor(7); // green
+    gfx_Line(-100, 20, 30, 40);
+
+    gfx_SetColor(224); // red
+    gfx_Line(30, 40, -100, 20); // same numbers as above, just flipped order
+
+    while (!os_GetCSC());
+
+    gfx_End();
+
+    return 0;
+}
