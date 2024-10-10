b118c543a3b97a8b80ca055e42cff1a39f03ed1b
unknown <71151164+ZERICO2005@users.noreply.github.com>
2024-10-10 17:32:36 -0600

    Improved the precision (and probably speed) of cbrtf. Should guarantee at least ~21.4 bits of precision.

--- src/libc/cbrt.c
+++ src/libc/cbrt.c
@@ -1,8 +1,53 @@
 #include <math.h>
 
-float cbrtf(float x)
+/**
+ * @remarks Newtons iteration will typically divide by 3.0, but I found that
+ * multiplying by ~0.3328 gives a more accurate result somehow. This is rather
+ * convienient since multiplication is faster than division.
+ */
+#define magic_third 0.3328f
+
+/**
+ * @remarks Minimum precision: 21.415037 bits at both +4.251052856e+02 and
+ * +7.969426579e+17 for cbrtf.
+ * The previous method of powf(x, 1.0f / 3.0f) had a minimum precision of
+ * 21.0 bits at +4.037431946e+02, and a minimum precision 19.299560 bits
+ * at +1.187444200e+07, slowly losing precision at larger magnitudes.
+ * @note Precision tested with 32bit floats on x86_64
+ */
+float cbrtf(const float x)
 {
-    return powf(x, 1/3.f);
+	if (x == 0.0f || !isfinite(x)) {
+		// Perserves signed zeros, inf, and NaN
+		return x;
+	}
+	const float fabs_x = fabsf(x);
+	const float fabs_x_mul_2 = 2.0f * x;
+
+	int expon = ilogbf(fabs_x);
+	
+	/**
+	 * @remarks Adds one for rounding purposes
+	 * @note trunc rounds in the opposite direction when expon is negative
+	 */
+	expon += (expon < 0) ? -1 : 1;
+	expon /= 3;
+
+	float guess = ldexpf(1.0f, expon);
+	float guess_cubed;
+
+	/* Halley iteration */
+	guess_cubed = guess * guess * guess;
+	guess = guess * ((guess_cubed + fabs_x_mul_2) / (2.0f * guess_cubed + fabs_x));
+
+	/* Newton iteration */
+	guess = magic_third * ((fabs_x / (guess * guess)) + 2.0f * guess);
+
+	/* Halley iteration */
+	guess_cubed = guess * guess * guess;
+	guess = guess * ((guess_cubed + fabs_x_mul_2) / (2.0f * guess_cubed + fabs_x));
+
+	return signbit(x) ? -guess : guess;
 }
 
 double cbrt(double) __attribute__((alias("cbrtf")));
