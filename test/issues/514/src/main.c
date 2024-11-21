67cb588058436e6a2e9954452ffbc5c4e1eec6f3
Adrien Bertrand <bertrand.adrien@gmail.com>
2024-11-21 11:04:03 +0100

    fix new strtrol test (don't display unstable pointers)

--- test/issues/514/src/main.c
+++ test/issues/514/src/main.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-
     os_ClrHome();
 
     char szNumbers[] = "2001 60c0c0 -1101110100110100100000 0x6fffff";
@@ -18,20 +17,22 @@ int main(void)
     li2 = strtol (pEnd,&pEnd,16);
     li3 = strtol (pEnd,&pEnd,2);
     li4 = strtol (pEnd,NULL,0);
-    printf("%ld\r\n", li1);
-    printf("%ld\r\n", li2);
-    printf("%ld\r\n", li3);
-    printf("%ld\r\n", li4);
+    printf("%ld\r\n", li1); // 2001
+    printf("%ld\r\n", li2); // 6340800
+    printf("%ld\r\n", li3); // -3624224
+    printf("%ld\r\n", li4); // 7340031
 
+    // Out of range test. but *endptr should still be 0, and endptr-str should be 13.
     const char* str = "1234567890123";
     char* endptr = NULL;
     long ret = strtol(str, &endptr, 10);
 
     printf("ret = %ld\r\n", ret);
-    printf("endptr = %p\r\n", endptr);
+    printf("endptr %s null\r\n", endptr ? "NOT" : "");
     printf("*endptr = %d\r\n", *endptr);
-    printf("str = %p\r\n", str);
     printf("endptr-str = %ld\r\n", (unsigned long)endptr-(unsigned long)str);
+
     os_GetKey();
+
     return 0;
 }
