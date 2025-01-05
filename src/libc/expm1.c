cd97828b078499b2b13f1da8dc01ae06f9285c4e
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-01-05 15:03:56 -0700

    Cleaned up expm1f code.

--- src/libc/expm1.c
+++ src/libc/expm1.c
@@ -1,24 +1,14 @@
 #include <math.h>
 
-float expm1f(const float x)
+float expm1f(float x)
 {
-    #if 1
-        /**
-         * 0x1.0p-12f: At least 12.0bits of precision
-         */
-        if (fabsf(x) < 2.44140625e-4f /* 0x1.0p-12f */) {
-            return x;
-        }
-    #else
-        /**
-         * 0x1.0p-8f: At least 16.0bits of precision
-         */
-        if (fabsf(x) < 3.90625e-3f /* 0x1.0p-8f */) {
-            return x + 0.5f * (x * x);
-        }
-    #endif
+    /**
+     * 0x1.0p-8f: At least 16.0bits of precision
+     */
+    if (fabsf(x) < 3.90625e-3f /* 0x1.0p-8f */) {
+        return x + 0.5f * (x * x);
+    }
     return expf(x) - 1.0f;
 }
-
 
 double expm1(double) __attribute__((alias("expm1f")));
