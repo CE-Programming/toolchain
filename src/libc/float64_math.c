ea33fd246b48d87538321b06201893c0565a1b66
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-01-14 23:20:22 -0700

    rewrote the float64-to-integer functions, and inlined some softfloat functions to reduce overhead

--- src/libc/float64_math.c
+++ src/libc/float64_math.c
@@ -9,6 +9,7 @@ typedef union F64_pun {
     uint64_t bin;
 } F64_pun;
 
+#if 0
 long double fmal(long double x, long double y, long double z) {
     F64_pun arg_x, arg_y, arg_z, ret;
     arg_x.flt = x;
@@ -17,6 +18,20 @@ long double fmal(long double x, long double y, long double z) {
     ret.soft = f64_mulAdd(arg_x.soft, arg_y.soft, arg_z.soft);
     return ret.flt;
 }
+#else
+float64_t softfloat_mulAddF64(
+	uint_fast64_t uiA, uint_fast64_t uiB, uint_fast64_t uiC, uint_fast8_t op
+);
+
+long double fmal(long double x, long double y, long double z) {
+    F64_pun arg_x, arg_y, arg_z, ret;
+    arg_x.flt = x;
+    arg_y.flt = y;
+    arg_z.flt = z;
+    ret.soft = softfloat_mulAddF64(arg_x.bin, arg_y.bin, arg_z.bin, 0);
+    return ret.flt;
+}
+#endif
 
 long double sqrtl(long double x) {
     F64_pun arg_x, ret;
