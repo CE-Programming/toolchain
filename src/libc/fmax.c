b85430287ae685566434929ae74e0ebcd2cfc233
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-01-21 21:41:24 -0700

    added C++20 numbers, merged ultod and ltod, along with some float64 math.h functions

--- src/libc/fmax.c
+++ src/libc/fmax.c
@@ -1,9 +1,25 @@
 #include <math.h>
 
-float fmaxf(float x, float y)
-{
-    if (x != y) return x < y ? y : x;
-    return signbit(x) || isnan(x) ? y : x;
+float fmaxf(float x, float y) {
+    return
+        isless(x, y) ? y :
+        isless(y, x) ? x :
+        /* attempts to return a non-NaN value */
+        isnan(x) ? y :
+        isnan(y) ? x :
+        /* arguments are equal or signed zero */
+        signbit(x) ? y : x;
 }
 
 double fmax(double, double) __attribute__((alias("fmaxf")));
+
+long double fmaxl(long double x, long double y) {
+    return
+        isless(x, y) ? y :
+        isless(y, x) ? x :
+        /* attempts to return a non-NaN value */
+        isnan(x) ? y :
+        isnan(y) ? x :
+        /* arguments are equal or signed zero */
+        signbit(x) ? y : x;
+}
