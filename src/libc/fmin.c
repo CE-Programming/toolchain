a81ff136291bd37fed8af91609140d8208a838b7
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-01-21 21:41:24 -0700

    added C++20 numbers, merged ultod and ltod, along with some float64 math.h functions

--- src/libc/fmin.c
+++ src/libc/fmin.c
@@ -1,9 +1,25 @@
 #include <math.h>
 
-float fminf(float x, float y)
-{
-    if (x != y) return x < y ? x : y;
-    return !signbit(x) || isnan(x) ? y : x;
+float fminf(float x, float y) {
+    return
+        isless(x, y) ? x :
+        isless(y, x) ? y :
+        /* attempts to return a non-NaN value */
+        isnan(x) ? y :
+        isnan(y) ? x :
+        /* arguments are equal or signed zero */
+        signbit(x) ? x : y;
 }
 
 double fmin(double, double) __attribute__((alias("fminf")));
+
+long double fminl(long double x, long double y) {
+    return
+        isless(x, y) ? x :
+        isless(y, x) ? y :
+        /* attempts to return a non-NaN value */
+        isnan(x) ? y :
+        isnan(y) ? x :
+        /* arguments are equal or signed zero */
+        signbit(x) ? x : y;
+}
