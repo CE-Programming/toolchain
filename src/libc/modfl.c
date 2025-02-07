d4cd9c8aab20e5e5b2364119edcdea9d7242473f
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-02-06 18:34:16 -0700

    Implemented float64 rounding functions

--- src/libc/modfl.c
+++ src/libc/modfl.c
@@ -1,30 +1,22 @@
-#include <fenv.h>
 #include <math.h>
 #include <stdint.h>
-#include "../softfloat/include/softfloat.h"
-
-typedef union F64_pun {
-    long double flt;
-    float64_t soft;
-    uint64_t bin;
-} F64_pun;
 
 long double modfl(long double x, long double *integral_part) {
-    if (iszero(x)) {
-        *integral_part = x;
-        return x;
+    switch (fpclassify(x)) {
+        case FP_ZERO:
+        case FP_NAN: {
+            *integral_part = x;
+            return x;
+        }
+        case FP_INFINITY: {
+            *integral_part = x;
+            return copysignl(0.0L, x);
+        }
+        case FP_SUBNORMAL:
+        case FP_NORMAL:
     }
-    if (isinf(x)) {
-        *integral_part = x;
-        return copysignl(0.0L, x);
-    }
-    F64_pun arg_x, arg_y, ret;
-    // modf does not set exceptions
-     __fenv_t temp_fe = __fe_cur_env;
-    arg_x.flt = x;
-    arg_y.flt = 1.0L;
-    *integral_part = truncl(x);
-    ret.soft = f64_rem(arg_x.soft, &arg_y.soft);
-    __fe_cur_env = temp_fe;
-    return ret.flt;
+    long double trunc_x = truncl(x);
+    *integral_part = x;
+    // result will be exact, and should not raise exceptions
+    return x - trunc_x;
 }
