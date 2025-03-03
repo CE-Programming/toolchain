2bfab3d4ed28e1bc55c274ee33a8355a1ebb8220
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-03-02 18:55:32 -0700

    Optimized sqrtf, added expl and logl, added constants for f32/f64, documented precision for exp/log functions

--- src/libc/expl.c
+++ src/libc/expl.c
@@ -12,57 +12,52 @@
 
 #include <errno.h>
 #include <math.h>
-#include "__float32_constants.h"
+#include "__float64_constants.h"
 
-/**
- * @note These coefficients are for float64
- */
-#define p0 0.20803843466947e7f
-#define p1 0.30286971697440e5f
-#define p2 0.60614853300611e2f
-#define q0 0.60027203602388e7f
-#define q1 0.32772515180829e6f
-#define q2 0.17492876890931e4f
+#define p0 0.20803843466947e7L
+#define p1 0.30286971697440e5L
+#define p2 0.60614853300611e2L
+#define q0 0.60027203602388e7L
+#define q1 0.32772515180829e6L
+#define q2 0.17492876890931e4L
 
 #if 0
-#define exp_max_arg 88.723f
-#define exp_min_arg -103.28f
+#define	exp_max_arg 709.79L
+#define exp_min_arg -744.45L
 #else
 /* mantissa is zero for these constants */
-#define exp_max_arg 128.0f
-#define exp_min_arg -128.0f
+#define exp_max_arg 1024.0L
+#define exp_min_arg -1024.0L
 #endif
 
 /**
- * @remarks Minimum ulp:
- * ulp of -4  at +0x1.439a98p-1 (-ln(2) < x < +ln(2))
- * ulp of -60 at +0x1.e2681ap+5 (-64.0f < x < +64.0f)
+ * @remarks Minimum relative precision of:
+ * 2^-45.23 at -4.062250536e-12 (-ln(2) < x < +ln(2))
+ * 2^-38.55 at -5.115436707e+02 (-512.0L < x < +512.0L)
  *
  * See the purple line for relative precision (lag warning):
  * https://www.desmos.com/calculator/zlrmxatxkf
  */
-float _expf_c(float arg) {
-    float fraction;
-    float temp1, temp2, xsq;
-    float ent;
+long double expl(long double arg) {
+    long double fraction;
+    long double temp1, temp2, xsq;
+    long double ent;
 
-    if ( arg == 0.0f){
-        return 1.0f;
+    if ( arg == 0.0L ){
+        return 1.0L;
     }
-    if ( arg < -exp_min_arg ){
-        return 0.0f;
+    if ( arg < exp_min_arg ){
+        return 0.0L;
     }
     if ( arg > exp_max_arg ){
         errno = ERANGE;
-        return HUGE_VALF;
+        return HUGE_VALL;
     }
-    arg *= F32_LOG2E;
-    ent = floorf( arg );
-    fraction = arg - ent - 0.5f;
+    arg *= F64_LOG2E;
+    ent = floorl( arg );
+    fraction = arg - ent - 0.5L;
     xsq = fraction * fraction;
     temp1 = ((p2 * xsq + p1) * xsq + p0) * fraction;
     temp2 = ((xsq + q2) * xsq + q1) * xsq + q0;
-    return ldexpf( F32_SQRT2 * (temp2+temp1) / (temp2-temp1), (int)ent );
+    return ldexpl( F64_SQRT2 * ((temp2+temp1) / (temp2-temp1)), (int)ent );
 }
-
-double _exp_c(double) __attribute__((alias("_expf_c")));
--- src/libc/sqrtf.c
+++ src/libc/sqrtf.c
@@ -1,29 +1,48 @@
 #include <errno.h>
 #include <math.h>
+#include <stdint.h>
 
+/*
+; only works for normalized values
+; CC: 9F + 12R + 9W + 1 | 8 bytes
+__f32_fast_div4:
+	pop	bc, hl, de
+	dec	e	; subtracts 2 from the exponent
+	push	de, hl, bc
+	ret
+*/
+float _f32_fast_div4(float x);
+
+/**
+ * @remarks Minimum ulp of -1
+ */
 float _sqrtf_c(float x)
 {
     float f, y;
     int n;
 
-    if (x==0.0) return x;
-    else if (x==1.0) return 1.0;
-    else if (x<0.0)
-    {
-        errno=EDOM;
-        return 0.0;
+    if (x == 0.0f) {
+        return x;
     }
-    f=frexpf(x, &n);
-    y=0.41731+0.59016*f; /*Educated guess*/
-    /*For a 24 bit mantisa (float), two iterations are sufficient*/
-    y+=f/y;
-    y=ldexpf(y, -2) + f/y; /*Faster version of 0.25 * y + f/y*/
+    if (x == 1.0f) {
+        return 1.0f;
+    }
+    if (x < 0.0f) {
+        errno = EDOM;
+        /* return a "NaN" value */
+        return 0.0f;
+    }
+    f = frexpf(x, &n);
+    y = 0.41731f + 0.59016f * f; /* Educated guess */
+    /* For a 24 bit mantisa (float), two iterations are sufficient */
+    y += f / y;
+    y = _f32_fast_div4(y) + f / y; /* Faster version of 0.25f * y + f/y */
 
-    if (n&1)
-    {
-        y*=0.7071067812;
+    if (n & 1) {
+        y *= (float)M_SQRT1_2;
         ++n;
     }
+    /* n will be [-148, 128] prior to division */
     return ldexpf(y, n/2);
 }
 
