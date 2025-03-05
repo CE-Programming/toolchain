b1eaf9bd17ef4ac8a6394fed179c604ec38af575
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-03-05 16:50:39 -0700

    Added atanl and atan2l, and did some formatting on atanf and atan2f

--- src/libc/atan2f.c
+++ src/libc/atan2f.c
@@ -1,27 +1,26 @@
 /* Copyright (c) 2000-2008 Zilog, Inc. */
 #include <math.h>
+#include "__float32_constants.h"
 
-#define pio2 1.57079632679489e0
-
-float _atan2f_c(float arg1,float arg2) {
+float _atan2f_c(float arg1, float arg2) {
     float satan(float);
 
-    if((arg1+arg2)==arg1) {
-        if(arg1 >= 0.) {
-            return(pio2);
+    if ((arg1+arg2)==arg1) {
+        if (arg1 >= 0.0f) {
+            return (F32_PI2);
         } else {
-            return(-pio2);
+            return (-F32_PI2);
         }
-    } else if(arg2 < 0.) {
-        if(arg1 >= 0.) {
-            return(pio2+pio2 - satan(-arg1/arg2));
+    } else if (arg2 < 0.0f) {
+        if(arg1 >= 0.0f) {
+            return (F32_PI - satan(-arg1/arg2));
         } else {
-            return(-pio2-pio2 + satan(arg1/arg2));
+            return (-F32_PI + satan(arg1/arg2));
         }
-    } else if(arg1 > 0) {
-        return(satan(arg1/arg2));
+    } else if (arg1 > 0.0f) {
+        return (satan(arg1/arg2));
     } else {
-        return(-satan(-arg1/arg2));
+        return (-satan(-arg1/arg2));
     }
 }
 
---
+++ src/libc/atan2l.c
@@ -0,0 +1,25 @@
+/* Copyright (c) 2000-2008 Zilog, Inc. */
+#include <math.h>
+#include "__float64_constants.h"
+
+long double atan2l(long double arg1, long double arg2) {
+    long double f64_satan(long double);
+
+    if ((arg1+arg2)==arg1) {
+        if (arg1 >= 0.0L) {
+            return (F64_PI2);
+        } else {
+            return (-F64_PI2);
+        }
+    } else if (arg2 < 0.0L) {
+        if(arg1 >= 0.0L) {
+            return (F64_PI - f64_satan(-arg1/arg2));
+        } else {
+            return (-F64_PI + f64_satan(arg1/arg2));
+        }
+    } else if (arg1 > 0.0L) {
+        return (f64_satan(arg1/arg2));
+    } else {
+        return (-f64_satan(-arg1/arg2));
+    }
+}
