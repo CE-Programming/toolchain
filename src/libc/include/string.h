aaf199df8e0d9a221ca0944d390874629f726743
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-03-22 01:06:25 -0600

    Added mempcpy and prototypes for additional <string.h> functions

--- src/libc/include/string.h
+++ src/libc/include/string.h
@@ -20,8 +20,17 @@ int memcmp(const void *s1, const void *s2, size_t n)
 void *memchr(const void *s, int c, size_t n)
     __attribute__((nonnull(1)));
 
-void* memccpy(void *__restrict dest, const void *__restrict src, int c, size_t n) __NOEXCEPT
-    __attribute__((nonnull(1, 2)));
+void *memrchr(const void *s, int c, size_t n)
+    __NOEXCEPT __attribute__((nonnull(1))) __attribute((__pure__));
+
+void *memmem(const void *haystack, size_t haystack_len, const void *needle, size_t needle_len)
+    __NOEXCEPT __attribute__((nonnull(1, 3))) __attribute((__pure__));
+
+void *memccpy(void *__restrict dest, const void *__restrict src, int c, size_t n)
+    __NOEXCEPT __attribute__((nonnull(1, 2)));
+
+void *mempcpy(void *__restrict dest, const void *__restrict src, size_t n)
+    __NOEXCEPT __attribute__((nonnull(1, 2)));
 
 char *strcpy(char *__restrict dest, const char *__restrict src)
     __attribute__((nonnull(1, 2)));
@@ -29,14 +38,14 @@ char *strcpy(char *__restrict dest, const char *__restrict src)
 char *strncpy(char *__restrict dest, const char *__restrict src, size_t n)
     __attribute__((nonnull(1, 2)));
 
-char *stpcpy(char *__restrict dest, const char *__restrict src) __NOEXCEPT
-    __attribute__((nonnull(1, 2)));
+char *stpcpy(char *__restrict dest, const char *__restrict src)
+    __NOEXCEPT __attribute__((nonnull(1, 2)));
 
-char *stpncpy(char *__restrict dest, const char *__restrict src, size_t n) __NOEXCEPT
-    __attribute__((nonnull(1, 2)));
+char *stpncpy(char *__restrict dest, const char *__restrict src, size_t n)
+    __NOEXCEPT __attribute__((nonnull(1, 2)));
 
-char *strlcpy(char *__restrict dest, const char *__restrict src, size_t n) __NOEXCEPT
-    __attribute__((nonnull(1, 2)));
+char *strlcpy(char *__restrict dest, const char *__restrict src, size_t n)
+    __NOEXCEPT __attribute__((nonnull(1, 2)));
 
 char *strcat(char *__restrict dest, const char *__restrict src)
     __attribute__((nonnull(1, 2)));
@@ -44,8 +53,8 @@ char *strcat(char *__restrict dest, const char *__restrict src)
 char *strncat(char *__restrict dest, const char *__restrict src, size_t n)
     __attribute__((nonnull(1, 2)));
 
-char *strlcat(char *__restrict dest, const char *__restrict src, size_t n) __NOEXCEPT
-    __attribute__((nonnull(1, 2)));
+char *strlcat(char *__restrict dest, const char *__restrict src, size_t n)
+    __NOEXCEPT __attribute__((nonnull(1, 2)));
 
 char *strchr(const char *s, int c)
     __attribute__((nonnull(1)));
@@ -58,6 +67,9 @@ char *strpbrk(const char *s, const char *accept)
 
 char *strstr(const char *haystack, const char *needle)
     __attribute__((nonnull(1, 2)));
+
+char *strcasestr(const char *haystack, const char *needle)
+    __NOEXCEPT __attribute__((nonnull(1, 2))) __attribute__((__pure__));
 
 char *strtok(char *__restrict s, const char *__restrict delim)
     __attribute__((nonnull(2)));
@@ -86,11 +98,11 @@ int strcmp(const char *s1, const char *s2)
 int strncmp(const char *s1, const char *s2, size_t n)
     __attribute__((nonnull(1, 2)));
 
-int strcasecmp(const char *s1, const char *s2) __NOEXCEPT
-    __attribute__((nonnull(1, 2))) __attribute__((__pure__));
+int strcasecmp(const char *s1, const char *s2)
+    __NOEXCEPT __attribute__((nonnull(1, 2))) __attribute__((__pure__));
 
-int strncasecmp(const char *s1, const char *s2, size_t n) __NOEXCEPT
-    __attribute__((nonnull(1, 2))) __attribute__((__pure__));
+int strncasecmp(const char *s1, const char *s2, size_t n)
+    __NOEXCEPT __attribute__((nonnull(1, 2))) __attribute__((__pure__));
 
 char* strerror(int errnum);
 
