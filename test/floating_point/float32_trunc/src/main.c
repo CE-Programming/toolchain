8f1ca3f6e6c208280c1afebba3ee4368645b7016
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-03-26 13:09:50 -0600

    Implemented modff and truncf in assembly

---
+++ test/floating_point/float32_trunc/src/main.c
@@ -0,0 +1,58 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <ti/screen.h>
+#include <ti/getcsc.h>
+#include <sys/util.h>
+
+#include "f32_trunc_LUT.h"
+
+#define ARRAY_LENGTH(x) (sizeof(x) / sizeof(x[0]))
+
+typedef union F32_pun {
+    float flt;
+    uint32_t bin;
+} F32_pun;
+
+size_t run_test(void) {
+    typedef F32_pun input_t;
+    typedef F32_pun output_t;
+
+    const size_t length = ARRAY_LENGTH(f32_trunc_LUT_input);
+    const input_t  *input  = (const input_t* )((const void*)f32_trunc_LUT_input );
+    const output_t *output = (const output_t*)((const void*)f32_trunc_LUT_output);
+
+    for (size_t i = 0; i < length; i++) {
+        F32_pun result;
+        result.flt = truncf(input[i].flt);
+        if (result.bin != output[i].bin && !(isnan(result.flt) || isnan(output[i].flt))) {
+            printf(
+                "I: %08lX T: %zu\nG: %08lX\nT: %08lX\n",
+                input[i].bin, i,
+                result.bin,
+                output[i].bin
+            );
+            return i;
+        }
+    }
+
+    /* passed all */
+    return SIZE_MAX;
+}
+
+int main(void) {
+    os_ClrHome();
+    size_t fail_index = run_test();
+    if (fail_index == SIZE_MAX) {
+        printf("All tests passed");
+    } else {
+        printf("Failed test: %zu", fail_index);
+    }
+
+    while (!os_GetCSC());
+
+    return 0;
+}
