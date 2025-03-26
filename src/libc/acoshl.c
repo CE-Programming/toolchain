5cbe12da476e1f1ece00402970d0ecf4682f964f
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-03-25 23:56:22 -0600

    added expm1l logp1l and improved acoshl atanhl

--- src/libc/acoshl.c
+++ src/libc/acoshl.c
@@ -1,9 +1,6 @@
 #include <math.h>
 #include "__float64_constants.h"
 
-/* enable when log1pl is implemented */
-#if 0
-
 /**
  * @remarks Minimum relative precision of:
  * 2^-42.37 at +1.000000358e+00 with ideal log1pl
@@ -15,14 +12,3 @@ long double acoshl(long double x) {
     }
     return logl(x) + F64_LN2;
 }
-
-#else
-
-long double acoshl(long double x) {
-    if (x < 0x1.0p+511L) {
-        return logl(x + sqrtl(x * x - 1.0L));
-    }
-    return logl(x) + F64_LN2;
-}
-
-#endif
