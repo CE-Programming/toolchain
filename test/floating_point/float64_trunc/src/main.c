5a3bc3c30a1170cde089383acdb84e66c7b1a2ea
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-04-12 18:09:09 -0600

    Implemented truncl in assembly

---
+++ test/floating_point/float64_trunc/src/main.c
@@ -0,0 +1,58 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <ti/screen.h>
+#include <ti/getcsc.h>
+#include <sys/util.h>
+
+#include "f64_trunc_LUT.h"
+
+#define ARRAY_LENGTH(x) (sizeof(x) / sizeof(x[0]))
+
+typedef union F64_pun {
+    long double flt;
+    uint64_t bin;
+} F64_pun;
+
+size_t run_test(void) {
+    typedef F64_pun input_t;
+    typedef F64_pun output_t;
+
+    const size_t length = ARRAY_LENGTH(f64_trunc_LUT_input);
+    const input_t  *input  = (const input_t* )((const void*)f64_trunc_LUT_input );
+    const output_t *output = (const output_t*)((const void*)f64_trunc_LUT_output);
+
+    for (size_t i = 0; i < length; i++) {
+        F64_pun result;
+        result.flt = truncl(input[i].flt);
+        if (result.bin != output[i].bin && !(isnan(result.flt) || isnan(output[i].flt))) {
+            printf(
+                "Test: %zu\nI: %016llX\nG: %016llX\nT: %016llX\n",
+                i,
+                input[i].bin,
+                result.bin,
+                output[i].bin
+            );
+            return i;
+        }
+    }
+    /* passed all */
+    return SIZE_MAX;
+}
+
+int main(void) {
+    os_ClrHome();
+    size_t fail_index = run_test();
+    if (fail_index == SIZE_MAX) {
+        printf("All tests passed");
+    } else {
+        printf("Failed test: %zu", fail_index);
+    }
+
+    while (!os_GetCSC());
+
+    return 0;
+}
