2f6389e6e8ca670208677346b9fb3a860d0c8a1c
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-04-21 22:15:24 -0600

    Added peephole optimization to graphy.c.src

--- src/graphy/graphy_strip.py
+++ src/graphy/graphy_strip.py
@@ -39,8 +39,127 @@ file_data = file_data.replace(b"_gfy_", b"gfy_")
 file_data = file_data.replace(b"_GFY_", b"GFY_")
 file_data = file_data.replace(b"_lcd_", b"lcd_")
 
+# This is the fun optimization section
+
+# __ishl
+
+file_data = file_data.replace(
+    b"\tld\tc, 1\n\tcall\t__ishl\n",
+    b"\tld\tc, 1\n\tadd\thl, hl\n"
+)
+file_data = file_data.replace(
+    b"\tld\tc, 2\n\tcall\t__ishl\n",
+    b"\tld\tc, 2\n\tadd\thl, hl\n\tadd\thl, hl\n"
+)
+file_data = file_data.replace(
+    b"\tld\tc, 3\n\tcall\t__ishl\n",
+    b"\tld\tc, 3\n\tadd\thl, hl\n\tadd\thl, hl\n\tadd\thl, hl\n"
+)
+file_data = file_data.replace(
+    b"\tld\tc, 4\n\tcall\t__ishl\n",
+    b"\tld\tc, 4\n\tadd\thl, hl\n\tadd\thl, hl\n\tadd\thl, hl\n\tadd\thl, hl\n"
+)
+
+# pop push __ishl
+
+file_data = file_data.replace(
+    b"\tld\tc, 1\n\tpush\tde\n\tpop\thl\n\tcall\t__ishl\n",
+    b"\tld\tc, 1\n\tpush\tde\n\tpop\thl\n\tadd\thl, hl\n",
+)
+
+file_data = file_data.replace(
+    b"\tld\tc, 1\n\tpush\tbc\n\tpop\thl\n\tcall\t__ishl\n",
+    b"\tld\tc, 1\n\tpush\tbc\n\tpop\thl\n\tadd\thl, hl\n",
+)
+
+# __bshl
+
+file_data = file_data.replace(
+    b"\tld\tb, 1\n\tcall\t__bshl\n",
+    b"\tld\tb, 1\n\tadd\ta, a\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tb, 2\n\tcall\t__bshl\n",
+    b"\tld\tb, 2\n\tadd\ta, a\n\tadd\ta, a\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tb, 3\n\tcall\t__bshl\n",
+    b"\tld\tb, 3\n\tadd\ta, a\n\tadd\ta, a\n\tadd\ta, a\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tb, 4\n\tcall\t__bshl\n",
+    b"\tld\tb, 4\n\tadd\ta, a\n\tadd\ta, a\n\tadd\ta, a\n\tadd\ta, a\n"
+)
+
+# __iand
+
+file_data = file_data.replace(
+    b"\tld\tbc, 255\n\tcall\t__iand\n",
+    b"\tld\tbc, 255\n\tinc.s\thl\n\tdec\thl\n\tld\th, b\n"
+)
+
+# __imulu
+
+file_data = file_data.replace(
+    b"\tld\tbc, 3\n\tcall\t__imulu\n",
+    b"\tpush\thl\n\tpop\tbc\n\tadd\thl, bc\n\tadd\thl, bc\n\tld\tbc, 3\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tbc, 240\n\tcall\t__imulu\n",
+    b"\tcall\t__set_bc_and_mul_hl_by_240\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tbc, -2\n\tcall\t__imulu\n",
+    b"\tcall\t__set_bc_and_mul_hl_by_minus2\n"
+)
+
+# targetted
+
+file_data = file_data.replace(
+    b"\tld\tbc, 240\n\tld\tde, 0\n\tld\te, (ix + 12)\n\tadd\tiy, de\n\tcall\t__imulu\n",
+    b"\tld\tde, 0\n\tld\te, (ix + 12)\n\tadd\tiy, de\n\tcall\t__set_bc_and_mul_hl_by_240\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tbc, 240\n\tadd\tiy, de\n\tld\thl, (ix - 9)\n\tcall\t__imulu\n",
+    b"\tadd\tiy, de\n\tld\thl, (ix - 9)\n\tcall\t__set_bc_and_mul_hl_by_240\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tbc, 240\n\tadd\tiy, de\n\tld\thl, (ix - 12)\n\tcall\t__imulu\n",
+    b"\tadd\tiy, de\n\tld\thl, (ix - 12)\n\tcall\t__set_bc_and_mul_hl_by_240\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tbc, 240\n\tld\ta, e\n\tld\tde, 0\n\tld\te, (ix + 12)\n\tadd\tiy, de\n\tcall\t__imulu\n",
+    b"\tld\ta, e\n\tld\tde, 0\n\tld\te, (ix + 12)\n\tadd\tiy, de\n\tcall\t__set_bc_and_mul_hl_by_240\n"
+)
+
+file_data = file_data.replace(
+    b"\tld\tde, 240\n\tpush\tbc\n\tpop\tiy\n\tpush\thl\n\tpop\tbc\n\tadd\tiy, bc\n\tld\thl, (ix + 9)\n\tpush\tde\n\tpop\tbc\n\tcall\t__imulu\n",
+    b"\t\n\tpush\tbc\n\tpop\tiy\n\tpush\thl\n\tpop\tbc\n\tadd\tiy, bc\n\tld\thl, (ix + 9)\n\tcall\t__set_bc_and_mul_hl_by_240\n"
+)
+
+# targetted second pass
+
+file_data = file_data.replace(
+    b"\tcall\t__set_bc_and_mul_hl_by_240\n\tpush\thl\n\tpop\tde\n\tadd\tiy, de\n\tlea\thl, iy\n",
+    b"\tcall\t__set_bc_and_mul_hl_by_240\n\tex\tde, hl\n\tadd\tiy, de\n\tlea\thl, iy\n"
+)
+
+file_data = file_data.replace(
+    b"\tcall\t__set_bc_and_mul_hl_by_240\n\tpush\thl\n\tpop\tde\n\tlea\thl, iy\n\tadd\thl, de\n",
+    b"\tcall\t__set_bc_and_mul_hl_by_240\n\tex\tde, hl\n\tlea\thl, iy\n\tadd\thl, de\n"
+)
+
 if (len(file_data) < 10):
     print("Error: Output is empty")
+
 
 with open(file_path, "wb") as file:
     file.write(file_data)
