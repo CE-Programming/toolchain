0cda91cb465bf87870677ffe6e06f62720c9de56
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-04-24 13:26:48 -0600

    Optimized memset and strlen, also added bzero

--- test/standalone/asprintf_fprintf/src/rename.asm
+++ test/standalone/asprintf_fprintf/src/rename.asm
@@ -4,6 +4,7 @@
 
 	public	_T_memset, _T_memcpy, _T_memcmp, _T_memccpy, _T_mempcpy
 	public	_T_strlen, _T_strcmp, _T_stpcpy
+	public	_T_bzero
 
 _T_memset := _memset
 _T_memcpy := _memcpy
@@ -15,5 +16,8 @@ _T_strlen := _strlen
 _T_strcmp := _strcmp
 _T_stpcpy := _stpcpy
 
+_T_bzero := _bzero
+
 	extern	_memset, _memcpy, _memcmp, _memccpy, _mempcpy
 	extern	_strlen, _strcmp, _stpcpy
+	extern	_bzero
