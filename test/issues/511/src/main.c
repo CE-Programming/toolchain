5c37119c0fc62c9156c1d7ef2ca46b177a90a4c2
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-05-07 13:45:56 -0600

    updated autotest CRC's to reflect updates to nanoprintf (mainly rounding overflow changes)

--- test/issues/511/src/main.c
+++ test/issues/511/src/main.c
@@ -9,8 +9,8 @@ int main(void)
 {
     static const uint8_t values[][4] =
     {
-        { 0xcf, 0x7f, 0x39, 0xbe },  /* -0.181151 */
-        { 0xcf, 0xff, 0x39, 0xbe },  /* -0.181639 */
+        { 0xcf, 0x7f, 0x39, 0xbe },  /* -0.18115161359 */
+        { 0xcf, 0xff, 0x39, 0xbe },  /* -0.18163998 */
         { 0x00, 0x00, 0x00, 0x00 },  /* +0 */
         { 0x00, 0x00, 0x00, 0x80 },  /* -0 */
         { 0x00, 0x00, 0x80, 0x7f },  /* +inf */
@@ -23,16 +23,16 @@ int main(void)
 
     /* expected output */
     /*
-        inf = N, NaN = N: -0.181151 (cf 7f 39 be)
-        inf = N, NaN = N: -0.181639 (cf ff 39 be)
-        inf = N, NaN = N: 0.000000 (00 00 00 00)
-        inf = N, NaN = N: -0.000000 (00 00 00 80)
-        inf = Y, NaN = N: inf (00 00 80 7f)
-        inf = Y, NaN = N: -inf (00 00 80 ff)
-        inf = N, NaN = Y: nan (00 01 80 7f)
-        inf = N, NaN = Y: -nan (00 01 80 ff)
-        inf = N, NaN = N: oor (00 00 10 7f)
-        inf = N, NaN = N: -oor (00 00 10 ff)
+        Test  1: inf = N, NaN = N: -0.181152 (cf 7f 39 be)
+        Test  2: inf = N, NaN = N: -0.181640 (cf ff 39 be)
+        Test  3: inf = N, NaN = N: 0.000000 (00 00 00 00)
+        Test  4: inf = N, NaN = N: -0.000000 (00 00 00 80)
+        Test  5: inf = Y, NaN = N: inf (00 00 80 7f)
+        Test  6: inf = Y, NaN = N: -inf (00 00 80 ff)
+        Test  7: inf = N, NaN = Y: nan (00 01 80 7f)
+        Test  8: inf = N, NaN = Y: -nan (00 01 80 ff)
+        Test  9: inf = N, NaN = N: err (00 00 10 7f)
+        Test 10: inf = N, NaN = N: -err (00 00 10 ff)
     */
 
     for (size_t i = 0; i < sizeof(values) / sizeof(*values); ++i)
