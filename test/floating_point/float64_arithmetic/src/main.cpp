30229fe703d14fa76eb6daded66ba069daa9a6d5
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-05-08 23:14:43 -0600

    implemented dcmp in assembly

--- test/floating_point/float64_arithmetic/src/main.cpp
+++ test/floating_point/float64_arithmetic/src/main.cpp
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <ti/screen.h>
 #include <ti/getcsc.h>
@@ -12,6 +13,12 @@
 #include <bit>
 #include <cmath>
 #include <numbers>
+
+#define AUTOTEST_DEBUG 0
+
+#ifndef AUTOTEST_DEBUG
+#define AUTOTEST_DEBUG 0
+#endif
 
 #include "f64_arithmetic_LUT.h"
 
@@ -71,31 +78,274 @@ extern volatile long double f64_neg_one;
 extern volatile long double f64_pos_pi;
 extern volatile long double f64_neg_pi;
 
+extern volatile long double f64_pos_tanx;
+extern volatile long double f64_neg_tanx;
+extern volatile long double f64_pos_tany;
+extern volatile long double f64_neg_tany;
+
 extern "C" {
 long double drem_libcall(long double x, long double y);
 }
 
 int comparison_test(void) {
-    C((f64_pos_one  == f64_pos_one ));
-    C((f64_neg_one  <  f64_pos_one ));
-    C((f64_pos_one  >= f64_neg_one ));
-    C((f64_pos_one  != f64_neg_one ));
-    C((f64_pos_zero <  f64_pos_one ));
-    C((f64_neg_zero >  f64_neg_one ));
+
+    /* equal values */
 
     C((f64_pos_zero == f64_pos_zero));
-    C((f64_neg_zero == f64_neg_zero));
-    C((f64_pos_zero >= f64_neg_zero));
-    C((f64_neg_zero == f64_pos_zero));
+    C((f64_pos_zero <= f64_pos_zero));
+    C((f64_pos_zero >= f64_pos_zero));
 
-    C((f64_pos_pi   == f64_pos_pi  ));
-    C((f64_pos_pi   >  f64_pos_one ));
-    C((f64_neg_pi   <  f64_neg_one ));
-    C((f64_pos_pi   >  f64_pos_zero));
-    C((f64_neg_pi   <= f64_neg_zero));
-    C((f64_neg_pi   != f64_neg_zero));
-    C((f64_pos_pi   != f64_neg_pi  ));
-    C((f64_pos_pi   >= f64_neg_pi  ));
+    C((f64_neg_zero == f64_neg_zero));
+    C((f64_neg_zero <= f64_neg_zero));
+    C((f64_neg_zero >= f64_neg_zero));
+
+    C((f64_pos_one == f64_pos_one));
+    C((f64_pos_one <= f64_pos_one));
+    C((f64_pos_one >= f64_pos_one));
+
+    C((f64_neg_one == f64_neg_one));
+    C((f64_neg_one <= f64_neg_one));
+    C((f64_neg_one >= f64_neg_one));
+
+    C((f64_pos_pi == f64_pos_pi));
+    C((f64_pos_pi <= f64_pos_pi));
+    C((f64_pos_pi >= f64_pos_pi));
+
+    C((f64_neg_pi == f64_neg_pi));
+    C((f64_neg_pi <= f64_neg_pi));
+    C((f64_neg_pi >= f64_neg_pi));
+
+    /* negated values */
+
+    C((f64_pos_zero == f64_neg_zero));
+    C((f64_pos_zero <= f64_neg_zero));
+    C((f64_pos_zero >= f64_neg_zero));
+
+    C((f64_neg_zero == f64_pos_zero));
+    C((f64_neg_zero <= f64_pos_zero));
+    C((f64_neg_zero >= f64_pos_zero));
+
+    C((f64_pos_one != f64_neg_one));
+    C((f64_pos_one >  f64_neg_one));
+    C((f64_pos_one >= f64_neg_one));
+
+    C((f64_neg_one != f64_pos_one));
+    C((f64_neg_one <  f64_pos_one));
+    C((f64_neg_one <= f64_pos_one));
+
+    C((f64_pos_pi  != f64_neg_pi ));
+    C((f64_pos_pi  >  f64_neg_pi ));
+    C((f64_pos_pi  >= f64_neg_pi ));
+
+    C((f64_neg_pi  != f64_pos_pi ));
+    C((f64_neg_pi  <  f64_pos_pi ));
+    C((f64_neg_pi  <= f64_pos_pi ));
+
+    /* compare to zero */
+
+    C((f64_pos_zero != f64_pos_one));
+    C((f64_pos_zero <  f64_pos_one));
+    C((f64_pos_zero <= f64_pos_one));
+
+    C((f64_pos_zero != f64_pos_pi ));
+    C((f64_pos_zero <  f64_pos_pi ));
+    C((f64_pos_zero <= f64_pos_pi ));
+
+    C((f64_neg_zero != f64_pos_one));
+    C((f64_neg_zero <  f64_pos_one));
+    C((f64_neg_zero <= f64_pos_one));
+
+    C((f64_neg_zero != f64_pos_pi ));
+    C((f64_neg_zero <  f64_pos_pi ));
+    C((f64_neg_zero <= f64_pos_pi ));
+
+    C((f64_pos_zero != f64_neg_one));
+    C((f64_pos_zero >  f64_neg_one));
+    C((f64_pos_zero >= f64_neg_one));
+
+    C((f64_pos_zero != f64_neg_pi ));
+    C((f64_pos_zero >  f64_neg_pi ));
+    C((f64_pos_zero >= f64_neg_pi ));
+
+    C((f64_neg_zero != f64_neg_one));
+    C((f64_neg_zero >  f64_neg_one));
+    C((f64_neg_zero >= f64_neg_one));
+
+    C((f64_neg_zero != f64_neg_pi ));
+    C((f64_neg_zero >  f64_neg_pi ));
+    C((f64_neg_zero >= f64_neg_pi ));
+
+    /* compare to one */
+
+    C((f64_pos_one != f64_pos_zero));
+    C((f64_pos_one >  f64_pos_zero));
+    C((f64_pos_one >= f64_pos_zero));
+
+    C((f64_pos_one != f64_pos_pi  ));
+    C((f64_pos_one <  f64_pos_pi  ));
+    C((f64_pos_one <= f64_pos_pi  ));
+
+    C((f64_neg_one != f64_pos_zero));
+    C((f64_neg_one <  f64_pos_zero));
+    C((f64_neg_one <= f64_pos_zero));
+
+    C((f64_neg_one != f64_pos_pi  ));
+    C((f64_neg_one <  f64_pos_pi  ));
+    C((f64_neg_one <= f64_pos_pi  ));
+
+    C((f64_pos_one != f64_neg_zero));
+    C((f64_pos_one >  f64_neg_zero));
+    C((f64_pos_one >= f64_neg_zero));
+
+    C((f64_pos_one != f64_neg_pi  ));
+    C((f64_pos_one >  f64_neg_pi  ));
+    C((f64_pos_one >= f64_neg_pi  ));
+
+    C((f64_neg_one != f64_neg_zero));
+    C((f64_neg_one <  f64_neg_zero));
+    C((f64_neg_one <= f64_neg_zero));
+
+    C((f64_neg_one != f64_neg_pi  ));
+    C((f64_neg_one >  f64_neg_pi  ));
+    C((f64_neg_one >= f64_neg_pi  ));
+
+    /* compare to pi */
+
+    C((f64_pos_pi != f64_pos_zero));
+    C((f64_pos_pi >  f64_pos_zero));
+    C((f64_pos_pi >= f64_pos_zero));
+
+    C((f64_pos_pi != f64_pos_one ));
+    C((f64_pos_pi >  f64_pos_one ));
+    C((f64_pos_pi >= f64_pos_one ));
+
+    C((f64_neg_pi != f64_pos_zero));
+    C((f64_neg_pi <  f64_pos_zero));
+    C((f64_neg_pi <= f64_pos_zero));
+
+    C((f64_neg_pi != f64_pos_one ));
+    C((f64_neg_pi <  f64_pos_one ));
+    C((f64_neg_pi <= f64_pos_one ));
+
+    C((f64_pos_pi != f64_neg_zero));
+    C((f64_pos_pi >  f64_neg_zero));
+    C((f64_pos_pi >= f64_neg_zero));
+
+    C((f64_pos_pi != f64_neg_one ));
+    C((f64_pos_pi >  f64_neg_one ));
+    C((f64_pos_pi >= f64_neg_one ));
+
+    C((f64_neg_pi != f64_neg_zero));
+    C((f64_neg_pi <  f64_neg_zero));
+    C((f64_neg_pi <= f64_neg_zero));
+
+    C((f64_neg_pi != f64_neg_one ));
+    C((f64_neg_pi <  f64_neg_one ));
+    C((f64_neg_pi <= f64_neg_one ));
+
+    return 0;
+}
+
+typedef union F64_pun {
+    long double flt;
+    uint64_t bin;
+} F64_pun;
+
+#define F64_CMP_EQUAL      0 /* sets the Z flag */
+#define F64_CMP_LESS      -1 /* sets the S flag */
+#define F64_CMP_GREATER    1 /* doesn't trigger flags */
+#define F64_CMP_UNORDERED  1 /* doesn't trigger flags */
+
+// assumes no NaN
+int dcmp(long double arg_x, long double arg_y) {
+    F64_pun x, y;
+    x.flt = arg_x;
+    y.flt = arg_y;
+
+    bool x_sign = signbit(x.flt);
+    bool y_sign = signbit(y.flt);
+    if (x_sign != y_sign) {
+        if (iszero(x.flt) && iszero(y.flt)) {
+            return F64_CMP_EQUAL;
+        }
+        return (x_sign ? F64_CMP_LESS : F64_CMP_GREATER);
+    }
+    
+    if (x.bin == y.bin) {
+        return F64_CMP_EQUAL;
+    }
+    return ((x.bin < y.bin) != x_sign) ? F64_CMP_LESS : F64_CMP_GREATER;
+}
+
+static uint64_t rand64(void) {
+    union {
+        uint64_t u64;
+        uint16_t u16[4];
+    } split;
+    split.u16[0] = (uint16_t)rand();
+    split.u16[1] = (uint16_t)rand();
+    split.u16[2] = (uint16_t)rand();
+    split.u16[3] = (uint16_t)rand();
+    return split.u64;
+}
+
+#if AUTOTEST_DEBUG
+void print_failed(size_t i, const char* s, uint64_t x, uint64_t y) {
+    printf(
+        "%zu: %s\nX: %016llX\nY: %016llX\n",
+        i, s, x, y
+    );
+}
+#else
+#define print_failed(...)
+#endif
+
+int random_comparison_test(void) {
+    srand(0x7184CE);
+    for (size_t i = 0; i < 128; i++) {
+        F64_pun x, y;
+        x.bin = rand64();
+        y.bin = rand64();
+        int cmp = dcmp(x.flt, y.flt);
+        if ((cmp == 0) != (x.flt == y.flt)) {
+            print_failed(i, "eq", x.bin, y.bin);
+            return __LINE__;
+        }
+        if ((cmp != 0) != (x.flt != y.flt)) {
+            print_failed(i, "ne", x.bin, y.bin);
+            return __LINE__;
+        }
+        if ((cmp < 0) != (x.flt < y.flt)) {
+            print_failed(i, "lt", x.bin, y.bin);
+            return __LINE__;
+        }
+        if ((cmp <= 0) != (x.flt <= y.flt)) {
+            print_failed(i, "le", x.bin, y.bin);
+            return __LINE__;
+        }
+        if ((cmp > 0) != (x.flt > y.flt)) {
+            print_failed(i, "gt", x.bin, y.bin);
+            return __LINE__;
+        }
+        if ((cmp >= 0) != (x.flt >= y.flt)) {
+            print_failed(i, "ge", x.bin, y.bin);
+            return __LINE__;
+        }
+    }
+    return 0;
+}
+
+int basic_test(void) {
+    {
+        int ret = comparison_test();
+        if (ret != 0) {
+            return ret;
+        }
+        ret = random_comparison_test();
+        if (ret != 0) {
+            return ret;
+        }
+    }
     
     /*  fmodl test */
     long double trunc_part;
@@ -123,7 +373,7 @@ int comparison_test(void) {
 
 int main(void) {
     os_ClrHome();
-    int comparison_result = comparison_test();
+    int comparison_result = basic_test();
     if (comparison_result != 0) {
         char buf[sizeof("Failed test L-8388608\n")];
         boot_sprintf(buf, "Failed test L%d\n", comparison_result);
