13b8b394007c51a97dfa72b4936e3eb3d6f1df41
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-06-17 15:59:17 -0600

    Updated the sprites_rotate_scale demo

--- examples/library_examples/graphx/sprites_rotate_scale/src/main.c
+++ examples/library_examples/graphx/sprites_rotate_scale/src/main.c
@@ -1,12 +1,23 @@
 #include <ti/getcsc.h>
 #include <graphx.h>
+#include <math.h>
+#include <time.h>
 
 /* Include the converted image data */
 #include "gfx/gfx.h"
 
+float cosine_interpolate(float t, float min_val, float max_val) {
+    /* Interpolates between min_val and max_val on a cosine wave */
+    /* t = 0 returns min_val */
+    float amplitude = (min_val - max_val) / 2.0f;
+    float offset = (min_val + max_val) / 2.0f;
+    return amplitude * cosf(t) + offset;
+}
+
 int main(void)
 {
-    uint8_t x = 0;
+    const int x_center = GFX_LCD_WIDTH / 2;
+    const int y_center = GFX_LCD_HEIGHT / 2;
 
     /* Initialize graphics drawing */
     gfx_Begin();
@@ -18,21 +29,35 @@ int main(void)
     /* Draw to buffer to avoid artifacts */
     gfx_SetDrawBuffer();
 
+    /* Record the start time */
+    const clock_t start_time = clock();
+
     /* Rotate the sprite until a key is pressed */
-    do
-    {
+    do {
+        /* Get the elasped time in seconds */
+        float time_elasped = (float)(clock() - start_time) / CLOCKS_PER_SEC;
+
+        /* Complete one rotation every 5 seconds */
+        uint8_t rotation = fmodf(time_elasped * 256.0f / 5.0f, 256.0f);
+
+        /* Interpolates between 75% and 250% scale (64 = 100% scale) */
+        uint8_t scale = (uint8_t)cosine_interpolate(time_elasped, 48, 160);
+
+        /* The output size of the sprite can be calculated with this formula */
+        uint8_t output_size = (scale * star_width) / 64;
+        
+        /* Calculate the x and y position to center the sprite */
+        int x_pos = x_center - output_size / 2;
+        int y_pos = y_center - output_size / 2;
 
         /* Draw a rotated transparent scaled spite */
-        gfx_RotatedScaledTransparentSprite_NoClip(star, 120, 80, 256 - x, 128);
+        gfx_RotatedScaledTransparentSprite_NoClip(star, x_pos, y_pos, rotation, scale);
 
         /* Show the buffered screen */
         gfx_BlitBuffer();
 
         /* Clear the old drawn sprite */
         gfx_FillScreen(1);
-
-        /* Change the rotation amount */
-        x++;
 
     } while (!os_GetCSC());
 
