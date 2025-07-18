8054b44a9abfcdcdaba3ba38bdfb8bb3496b9726
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-07-17 18:07:09 -0600

    ldexpf now correctly handles/rounds subnormal inputs

--- test/floating_point/float32_ldexp/src/main.c
+++ test/floating_point/float32_ldexp/src/main.c
@@ -11,7 +11,6 @@
 
 #include "f32_ldexp_LUT.h"
 
-
 #define ARRAY_LENGTH(x) (sizeof(x) / sizeof(x[0]))
 
 typedef union F32_pun {
@@ -20,7 +19,7 @@ typedef union F32_pun {
 } F32_pun;
 
 size_t run_test(void) {
-    typedef struct { float value; int expon; } input_t;
+    typedef struct { F32_pun value; int expon; } input_t;
     typedef F32_pun output_t;
 
     const size_t length = ARRAY_LENGTH(f32_ldexp_LUT_input);
@@ -28,25 +27,24 @@ size_t run_test(void) {
     const output_t *output = (const output_t*)((const void*)f32_ldexp_LUT_output);
     for (size_t i = 0; i < length; i++) {
         F32_pun result;
-        result.flt = ldexpf(input[i].value, input[i].expon);
+        result.flt = ldexpf(input[i].value.flt, input[i].expon);
         if (result.bin != output[i].bin) {
-            // ignore NaN's with differing payloads
-            // treat signed zeros as equal for now
             if (
+                /* ignore NaN's with differing payloads */
                 (!(isnan(result.flt) && isnan(output[i].flt))) &&
-                (!(result.bin == 0 && iszero(output[i].flt)))
+                #if 1
+                    /* treat signed zeros as equal for now */
+                    (!(result.bin == 0 && iszero(output[i].flt)))
+                #endif
             ) {
-                /* Float multiplication does not handle subnormals yet */
-                if (!(iszero(result.flt) && (issubnormal(output[i].flt) || issubnormal(input[i].value)))) {
-                    #if 1
-                        printf(
-                            "%zu:\nI: %08lX %+d\nG: %08lX\nT: %08lX\n",
-                            i, *(uint32_t*)(void*)&(input[i].value), input[i].expon,
-                            result.bin, output[i].bin
-                        );
-                    #endif
-                    return i;
-                }
+                #if 0
+                    printf(
+                        "%zu:\nI: %08lX %+d\nG: %08lX\nT: %08lX\n",
+                        i, input[i].value.bin, input[i].expon,
+                        result.bin, output[i].bin
+                    );
+                #endif
+                return i;
             }
         }
     }
