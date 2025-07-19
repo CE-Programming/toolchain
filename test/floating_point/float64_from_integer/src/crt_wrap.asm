529c5587e66a737d25e84d319f812744e2b6af4d
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-07-19 16:20:02 -0600

    (u)lltod can now raise FE_INEXACT (disabled by default). Removed (u)itod for now

--- test/floating_point/float64_from_integer/src/crt_wrap.asm
+++ test/floating_point/float64_from_integer/src/crt_wrap.asm
@@ -1,14 +1,32 @@
 	assume	adl=1
 
+;-------------------------------------------------------------------------------
+
 	section	.text
 
-	public	_CRT_utod, _CRT_itod
+	public	_clear_fe_cur_env
+_clear_fe_cur_env:
+	ld	a, (___fe_cur_env)
+	and	a, -125	; feclearexcept(FE_ALL_EXCEPT)
+	ld	(___fe_cur_env), a
+	ret
 
-_CRT_utod:
+	public	_get_fe_cur_env
+_get_fe_cur_env:
+	ld	a, (___fe_cur_env)
+	ret
+
+;-------------------------------------------------------------------------------
+
+	section	.text
+
+	public	_CRT_uitod, _CRT_itod
+
+_CRT_uitod:
 	ld	hl, 3
 	add	hl, sp
 	ld	hl, (hl)
-	jp	__utod
+	jp	__uitod
 
 _CRT_itod:
 	ld	hl, 3
@@ -16,5 +34,11 @@ _CRT_itod:
 	ld	hl, (hl)
 	jp	__itod
 
-	extern	__utod
+;-------------------------------------------------------------------------------
+
+	extern	__ultod
+	extern	__ltod
+	extern	___fe_cur_env
+
+	extern	__uitod
 	extern	__itod
