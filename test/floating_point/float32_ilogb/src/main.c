0106e838579d2a8a9a8598de44bb3d9a07c3dcbe
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-08-30 11:52:18 -0600

    implemented logbf in asm and optimized ilogbf and copysignl

--- test/floating_point/float32_ilogb/src/main.c
+++ test/floating_point/float32_ilogb/src/main.c
@@ -2,7 +2,10 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
+#include <errno.h>
+#include <fenv.h>
 #include <assert.h>
 #include <ti/screen.h>
 #include <ti/getcsc.h>
@@ -12,6 +15,25 @@
 #include "f32_ilogb_LUT.h"
 
 #define ARRAY_LENGTH(x) (sizeof(x) / sizeof(x[0]))
+
+#if 0
+#define test_printf printf
+#else
+#define test_printf(...)
+#endif
+
+float truth_logbf(float x) {
+    if (isfinite(x)) {
+        if (iszero(x)) {
+            errno = EDOM;
+            feraiseexcept(FE_DIVBYZERO);
+            return -HUGE_VALF;
+        }
+        return (float)ilogbf(x);
+    }
+    // infinity and NaN
+    return fabsf(x);
+}
 
 size_t run_test(void) {
     typedef float input_t;
@@ -24,9 +46,32 @@ size_t run_test(void) {
     for (size_t i = 0; i < length; i++) {
         int result = ilogbf(input[i]);
         if (result != output[i]) {
-            #if 0
-                printf("%3zu: %08lX\n\t%d != %d\n", i, input[i], result, output[i]);
-            #endif
+            test_printf("%3zu: %08lX\n%d != %d\n", i, input[i], result, output[i]);
+            return i;
+        }
+
+        errno = 0;
+        feclearexcept(FE_ALL_EXCEPT);
+        float f_truth = truth_logbf(input[i]);
+        unsigned char fe_truth = __fe_cur_env;
+        int errno_truth = errno;
+
+        errno = 0;
+        feclearexcept(FE_ALL_EXCEPT);
+        float f_guess = logbf(input[i]);
+        unsigned char fe_guess = __fe_cur_env;
+        int errno_guess = errno;
+
+        if (memcmp(&f_guess, &f_truth, sizeof(float)) != 0) {
+            test_printf("%3zu: %08lX %d\n%08lX != %08lX\n", i, input[i], output[i], f_guess, f_truth);
+            return i;
+        }
+        if (fe_guess != fe_truth) {
+            fputs("fenv fail\n", stdout);
+            return i;
+        }
+        if (errno_guess != errno_truth) {
+            fputs("errno fail\n", stdout);
             return i;
         }
     }
--- test/floating_point/float64_ilogb/src/main.c
+++ test/floating_point/float64_ilogb/src/main.c
@@ -2,7 +2,10 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
+#include <errno.h>
+#include <fenv.h>
 #include <assert.h>
 #include <ti/screen.h>
 #include <ti/getcsc.h>
@@ -12,6 +15,25 @@
 #include "f64_ilogb_LUT.h"
 
 #define ARRAY_LENGTH(x) (sizeof(x) / sizeof(x[0]))
+
+#if 0
+#define test_printf printf
+#else
+#define test_printf(...)
+#endif
+
+long double truth_logbl(long double x) {
+    if (isfinite(x)) {
+        if (iszero(x)) {
+            errno = EDOM;
+            feraiseexcept(FE_DIVBYZERO);
+            return -HUGE_VALL;
+        }
+        return (long double)ilogbl(x);
+    }
+    // infinity and NaN
+    return fabsl(x);
+}
 
 size_t run_test(void) {
     typedef long double input_t;
@@ -24,9 +46,32 @@ size_t run_test(void) {
     for (size_t i = 0; i < length; i++) {
         int result = ilogbl(input[i]);
         if (result != output[i]) {
-            #if 0
-                printf("%4zu: %016llX\n %d != %d\n", i, input[i], result, output[i]);
-            #endif
+            test_printf("%4zu: %016llX\n%d != %d\n", i, input[i], result, output[i]);
+            return i;
+        }
+
+        errno = 0;
+        feclearexcept(FE_ALL_EXCEPT);
+        long double f_truth = truth_logbl(input[i]);
+        unsigned char fe_truth = __fe_cur_env;
+        int errno_truth = errno;
+
+        errno = 0;
+        feclearexcept(FE_ALL_EXCEPT);
+        long double f_guess = logbl(input[i]);
+        unsigned char fe_guess = __fe_cur_env;
+        int errno_guess = errno;
+
+        if (memcmp(&f_guess, &f_truth, sizeof(long double)) != 0) {
+            test_printf("%4zu:\n%016llX %d\n%016llX !=\n%016llX\n", i, input[i], output[i], f_guess, f_truth);
+            return i;
+        }
+        if (fe_guess != fe_truth) {
+            fputs("fenv fail\n", stdout);
+            return i;
+        }
+        if (errno_guess != errno_truth) {
+            fputs("errno fail\n", stdout);
             return i;
         }
     }
