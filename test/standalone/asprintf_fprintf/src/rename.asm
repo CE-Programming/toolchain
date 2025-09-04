6018d315c92562925f15d4f7947a5ff162aa67a6
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-09-04 13:35:22 -0600

    optimized memmove

--- test/standalone/asprintf_fprintf/src/rename.asm
+++ test/standalone/asprintf_fprintf/src/rename.asm
@@ -2,12 +2,13 @@
 
 	section	.text
 
-	public	_T_memset, _T_memcpy, _T_memcmp, _T_memccpy, _T_mempcpy, _T_memrchr
+	public	_T_memset, _T_memcpy, _T_memmove, _T_memcmp, _T_memccpy, _T_mempcpy, _T_memrchr
 	public	_T_strlen, _T_strcmp, _T_strncmp, _T_stpcpy
 	public	_T_bzero
 
 _T_memset := _memset
 _T_memcpy := _memcpy
+_T_memmove := _memmove
 _T_memcmp := _memcmp
 _T_memccpy := _memccpy
 _T_mempcpy := _mempcpy
@@ -26,6 +27,6 @@ _T_bzero := _bzero
 _NULL_ptr:
 	db	$00, $00, $00
 
-	extern	_memset, _memcpy, _memcmp, _memccpy, _mempcpy, _memrchr
+	extern	_memset, _memcpy, _memmove, _memcmp, _memccpy, _mempcpy, _memrchr
 	extern	_strlen, _strcmp, _strncmp, _stpcpy
 	extern	_bzero
