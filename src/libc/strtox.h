b6e2c97da8372311d629d6c0ac759e7b2c17800c
Matthew Waltz <matthewwaltzis@gmail.com>
2025-09-04 20:47:57 -0600

    update copyright

--- src/libc/strtox.h
+++ src/libc/strtox.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2024 CE Programming
+ * Copyright (C) 2015-2025 CE Programming
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
