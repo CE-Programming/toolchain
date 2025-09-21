d190d5594dd992a17dc40b2ef1b2f274fed47716
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-09-21 16:00:22 -0600

    optimized call/load relative

--- src/libload/libload.asm
+++ src/libload/libload.asm
@@ -145,9 +145,7 @@ end macro
 relocate helpers, buf + 900
 call_relative:
 	pop	ix
-	lea	ix, ix + 3
-	push	ix
-	lea	ix, ix - 3
+	pea	ix + 3
 	push	de
 	ld	de, (ix)
 	add	ix, de
@@ -161,14 +159,14 @@ jump_relative:
 	pop	de
 	jp	(ix)
 ld_relative:
-	pop	ix
-	ld	de, (ix)
-	push	ix
-	add	ix, de
-	lea	hl, ix
-	pop	ix
-	lea	ix, ix + 3
-	jp	(ix)
+	pop	hl
+	ld	de, (hl)
+	inc	hl
+	inc	hl
+	inc	hl
+	push	hl
+	add	hl, de			; add hl, relative - 3
+	ret
 end relocate
 
 macro rcall? name
@@ -183,7 +181,7 @@ end macro
 
 macro rload? name
 	call	ld_relative
-	dl	name - $
+	dl	name - $ - 3
 end macro
 
 start:
