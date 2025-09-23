a4eaf15e9c859ac2028dbd082f872ea9470e6f5f
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-09-23 11:54:56 -0600

    implemented strto(u)l in assembly

--- src/libc/strtoull.c
+++ src/libc/strtoull.c
@@ -1,6 +1,6 @@
 #define STRTOX_TYPE    long long
 #define STRTOX_MAX     ULLONG_MAX
-#define STRTOX_MIN     0
+#define STRTOX_MIN     ULLONG_MAX
 
 #define STRTOX_SIGNED  0
 #define STRTOX_NAME    strtoull
