a3d855f0082b3c2fcc214bf629f49b01b2b0b38a
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-10-08 10:52:44 -0600

    added stpncpy

--- test/standalone/asprintf_fprintf/src/rename.asm
+++ test/standalone/asprintf_fprintf/src/rename.asm
@@ -3,7 +3,7 @@
 	section	.text
 
 	public	_T_memset, _T_memcpy, _T_memmove, _T_memcmp, _T_memccpy, _T_mempcpy, _T_memrchr
-	public	_T_strlen, _T_strcmp, _T_strncmp, _T_stpcpy, _T_strlcat
+	public	_T_strlen, _T_strcmp, _T_strncmp, _T_stpcpy, _T_stpncpy, _T_strlcat
 	public	_T_bzero
 
 _T_memset := _memset
@@ -18,6 +18,7 @@ _T_strlen := _strlen
 _T_strcmp := _strcmp
 _T_strncmp := _strncmp
 _T_stpcpy := _stpcpy
+_T_stpncpy := _stpncpy
 _T_strlcat := _strlcat
 
 _T_bzero := _bzero
@@ -29,5 +30,5 @@ _NULL_ptr:
 	db	$00, $00, $00
 
 	extern	_memset, _memcpy, _memmove, _memcmp, _memccpy, _mempcpy, _memrchr
-	extern	_strlen, _strcmp, _strncmp, _stpcpy, _strlcat
+	extern	_strlen, _strcmp, _strncmp, _stpcpy, _stpncpy, _strlcat
 	extern	_bzero
