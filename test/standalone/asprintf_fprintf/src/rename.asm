8c1312a5cd575c94085ae4c81fa32d23437a98eb
zerico2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-10-14 11:51:48 -0600

    added memmem

--- test/standalone/asprintf_fprintf/src/rename.asm
+++ test/standalone/asprintf_fprintf/src/rename.asm
@@ -2,7 +2,7 @@
 
 	section	.text
 
-	public	_T_memset, _T_memcpy, _T_memmove, _T_memcmp, _T_memccpy, _T_mempcpy, _T_memrchr
+	public	_T_memset, _T_memcpy, _T_memmove, _T_memcmp, _T_memccpy, _T_mempcpy, _T_memrchr, _T_memmem
 	public	_T_strlen, _T_strcmp, _T_strncmp, _T_stpcpy, _T_stpncpy, _T_strlcat
 	public	_T_bzero
 
@@ -13,6 +13,7 @@ _T_memcmp := _memcmp
 _T_memccpy := _memccpy
 _T_mempcpy := _mempcpy
 _T_memrchr := _memrchr
+_T_memmem := _memmem
 
 _T_strlen := _strlen
 _T_strcmp := _strcmp
@@ -29,6 +30,6 @@ _T_bzero := _bzero
 _NULL_ptr:
 	db	$00, $00, $00
 
-	extern	_memset, _memcpy, _memmove, _memcmp, _memccpy, _mempcpy, _memrchr
+	extern	_memset, _memcpy, _memmove, _memcmp, _memccpy, _mempcpy, _memrchr, _memmem
 	extern	_strlen, _strcmp, _strncmp, _stpcpy, _stpncpy, _strlcat
 	extern	_bzero
