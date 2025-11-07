7a29d998cef3d35c5eb919192f2136350b72e635
zerico2005 <71151164+ZERICO2005@users.noreply.github.com>
2025-11-07 14:22:42 -0700

    fixed behavior of memset((void*)-1, ch, 0) along with bzero and wmemset

--- test/standalone/wide_char/src/rename.asm
+++ test/standalone/wide_char/src/rename.asm
@@ -9,3 +9,20 @@ _PTR_000000:
 	public	_PTR_FFFFFF
 _PTR_FFFFFF:
 	db	$FF, $FF, $FF
+
+	section	.text
+
+	public	_T_wmemset, _T_wmemcpy, _T_wmemmove, _T_wmemcmp, _T_wmemchr
+	public	_T_wcslen, _T_wcsnlen
+
+_T_wmemset := _wmemset
+_T_wmemcpy := _wmemcpy
+_T_wmemmove := _wmemmove
+_T_wmemcmp := _wmemcmp
+_T_wmemchr := _wmemchr
+
+_T_wcslen := _wcslen
+_T_wcsnlen := _wcsnlen
+
+	extern	_wmemset, _wmemcpy, _wmemmove, _wmemcmp, _wmemchr
+	extern	_wcslen, _wcsnlen
