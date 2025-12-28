8ff99375b6adb65e86a7069f6d27877aba630fa0
Matthew Waltz <matthewwaltzis@gmail.com>
2025-12-28 07:10:20 -0700

    [libc] fix some issues with asctime

--- src/libc/asctime.c
+++ src/libc/asctime.c
@@ -14,7 +14,19 @@ char *asctime(const struct tm *timeptr)
     };
     static char result[26];
 
-    sprintf(result, "%.3s %.3s%3d %.2d:%.2d:%.2d %d\n",
+    if (!timeptr ||
+        timeptr->tm_wday < 0 || timeptr->tm_wday > 6  ||
+        timeptr->tm_mon < 0  || timeptr->tm_mon > 11  ||
+        timeptr->tm_mday < 1 || timeptr->tm_mday > 31 ||
+        timeptr->tm_hour < 0 || timeptr->tm_hour > 23 ||
+        timeptr->tm_min < 0  || timeptr->tm_min > 59  ||
+        timeptr->tm_sec < 0  || timeptr->tm_sec > 60  ||
+        timeptr->tm_year < -1900 || timeptr->tm_year > 8099)
+    {
+        return NULL;
+    }
+
+    sprintf(result, "%.3s %.3s %2d %.2d:%.2d:%.2d %d\n",
         wday_name[timeptr->tm_wday],
         mon_name[timeptr->tm_mon],
         timeptr->tm_mday,
