f6db693121b17d2ee2d1a663f022f837a6330818
Brendan Fletcher <calc84maniac@gmail.com>
2026-01-31 13:59:30 -0500

    Implement LZ4 block decompression

--- src/ce/include/compression.h
+++ src/ce/include/compression.h
@@ -8,6 +8,8 @@
 #ifndef COMPRESSION_H
 #define COMPRESSION_H
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -28,8 +30,27 @@ void zx7_Decompress(void *dst, const void *src);
 */
 void zx0_Decompress(void *dst, const void *src);
 
+/**
+ * Decompress a length-prefixed block of LZ4 encoded data.
+ *
+ * @param[in] dst Uncompressed data destination.
+ * @param[in] src Compressed data source.
+*/
+void lz4_Decompress(void *dst, const void *src);
+
+/**
+ * Decompress a raw block of LZ4 encoded data.
+ *
+ * @param[in] dst Uncompressed data destination.
+ * @param[in] src Compressed data source start.
+ * @param[in] size Compressed data source size.
+ *
+ * @returns The uncompressed data size.
+*/
+size_t lz4_Decompress_Block(void *dst, const void *src, size_t size);
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif /* COMPRESSION_H */
