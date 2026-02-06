3b4bba53322ec9c7be9d081c9769de4afde37ee4
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-02-06 14:40:01 -0700

    switch tests to iostream fully

--- test/standalone/iostream/src/main.cpp
+++ test/standalone/iostream/src/main.cpp
@@ -5,12 +5,18 @@
 #include <cstdio>
 #include <iostream>
 
+extern "C" void init_iostream(void);
+
 int main(void) {
+    init_iostream();
+
     os_ClrHome();
 
     int value = 123;
+    printf("start: %d\n", value);
+
     volatile bool* test = (volatile bool*)0xE40000;
-    if (*test) {
+    if (true || *test) {
         std::cout << value;
     }
 
