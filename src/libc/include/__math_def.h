7d0514c2b060acacdaa34e04bdf1ba41ea9e9c04
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-02-06 18:25:41 -0700

    DEBUG DO NOT USE| test if __builtin_* crashes anything

--- src/libc/include/__math_def.h
+++ src/libc/include/__math_def.h
@@ -69,6 +69,8 @@ bool _signbitf(float) __NOEXCEPT_CONST;
 bool _signbitl(long double) __NOEXCEPT_CONST;
 #endif
 
+#ifdef DEBUG_FOOBAR
+
 double      acos(double);
 float       acosf(float);
 long double acosl(long double);
@@ -172,9 +174,9 @@ double      fmod(double, double);
 float       fmodf(float, float);
 long double fmodl(long double, long double);
 
-double      frexp(double, int *) __attribute__((nonnull(2)));
-float       frexpf(float, int *) __attribute__((nonnull(2)));
-long double frexpl(long double, int *) __attribute__((nonnull(2)));
+double      frexp(double, int *) ;
+float       frexpf(float, int *) ;
+long double frexpl(long double, int *) ;
 
 double      hypot(double, double);
 float       hypotf(float, float);
@@ -232,9 +234,9 @@ long        lround(double);
 long        lroundf(float);
 long        lroundl(long double);
 
-double      modf(double, double *) __attribute__((nonnull(2)));
-float       modff(float, float *) __attribute__((nonnull(2)));
-long double modfl(long double, long double *) __attribute__((nonnull(2)));
+double      modf(double, double *) ;
+float       modff(float, float *) ;
+long double modfl(long double, long double *) ;
 
 double      nan(const char *);
 float       nanf(const char *);
@@ -268,9 +270,9 @@ double      remainder(double, double);
 float       remainderf(float, float);
 long double remainderl(long double, long double);
 
-double      remquo(double, double, int *) __attribute__((nonnull(3)));
-float       remquof(float, float, int *) __attribute__((nonnull(3)));
-long double remquol(long double, long double, int *) __attribute__((nonnull(3)));
+double      remquo(double, double, int *) ;
+float       remquof(float, float, int *) ;
+long double remquol(long double, long double, int *) ;
 
 double      rint(double);
 float       rintf(float);
@@ -327,6 +329,269 @@ long double tgammal(long double);
 double      trunc(double);
 float       truncf(float);
 long double truncl(long double);
+
+#else
+
+static inline double      acos(double x) { return __builtin_acos(x); }
+static inline float       acosf(float x) { return __builtin_acosf(x); }
+static inline long double acosl(long double x) { return __builtin_acosl(x); }
+
+static inline double      acosh(double x) { return __builtin_acosh(x); }
+static inline float       acoshf(float x) { return __builtin_acoshf(x); }
+static inline long double acoshl(long double x) { return __builtin_acoshl(x); }
+
+static inline double      asin(double x) { return __builtin_asin(x); }
+static inline float       asinf(float x) { return __builtin_asinf(x); }
+static inline long double asinl(long double x) { return __builtin_asinl(x); }
+
+static inline double      asinh(double x) { return __builtin_asinh(x); }
+static inline float       asinhf(float x) { return __builtin_asinhf(x); }
+static inline long double asinhl(long double x) { return __builtin_asinhl(x); }
+
+static inline double      atan(double x) { return __builtin_atan(x); }
+static inline float       atanf(float x) { return __builtin_atanf(x); }
+static inline long double atanl(long double x) { return __builtin_atanl(x); }
+
+static inline double      atan2(double y, double x) { return __builtin_atan2(y, x); }
+static inline float       atan2f(float y, float x) { return __builtin_atan2f(y, x); }
+static inline long double atan2l(long double y, long double x) { return __builtin_atan2l(y, x); }
+
+static inline double      atanh(double x) { return __builtin_atanh(x); }
+static inline float       atanhf(float x) { return __builtin_atanhf(x); }
+static inline long double atanhl(long double x) { return __builtin_atanhl(x); }
+
+static inline double      cbrt(double x) { return __builtin_cbrt(x); }
+static inline float       cbrtf(float x) { return __builtin_cbrtf(x); }
+static inline long double cbrtl(long double x) { return __builtin_cbrtl(x); }
+
+static inline double      ceil(double x) { return __builtin_ceil(x); }
+static inline float       ceilf(float x) { return __builtin_ceilf(x); }
+static inline long double ceill(long double x) { return __builtin_ceill(x); }
+
+static inline double      copysign(double x, double y) { return __builtin_copysign(x, y); }
+static inline float       copysignf(float x, float y) { return __builtin_copysignf(x, y); }
+static inline long double copysignl(long double x, long double y) { return __builtin_copysignl(x, y); }
+
+static inline double      cos(double x) { return __builtin_cos(x); }
+static inline float       cosf(float x) { return __builtin_cosf(x); }
+static inline long double cosl(long double x) { return __builtin_cosl(x); }
+
+double      cospi(double);
+float       cospif(float);
+long double cospil(long double);
+
+static inline double      cosh(double x) { return __builtin_cosh(x); }
+static inline float       coshf(float x) { return __builtin_coshf(x); }
+static inline long double coshl(long double x) { return __builtin_coshl(x); }
+
+static inline double      erf(double x) { return __builtin_erf(x); }
+static inline float       erff(float x) { return __builtin_erff(x); }
+static inline long double erfl(long double x) { return __builtin_erfl(x); }
+
+static inline double      erfc(double x) { return __builtin_erfc(x); }
+static inline float       erfcf(float x) { return __builtin_erfcf(x); }
+static inline long double erfcl(long double x) { return __builtin_erfcl(x); }
+
+static inline double      exp(double x) { return __builtin_exp(x); }
+static inline float       expf(float x) { return __builtin_expf(x); }
+static inline long double expl(long double x) { return __builtin_expl(x); }
+
+static inline double      exp2(double x) { return __builtin_exp2(x); }
+static inline float       exp2f(float x) { return __builtin_exp2f(x); }
+static inline long double exp2l(long double x) { return __builtin_exp2l(x); }
+
+static inline double      expm1(double x) { return __builtin_expm1(x); }
+static inline float       expm1f(float x) { return __builtin_expm1f(x); }
+static inline long double expm1l(long double x) { return __builtin_expm1l(x); }
+
+#ifndef _ABS_FLOAT_DEFINED
+#define _ABS_FLOAT_DEFINED
+double      fabs(double x);
+float       fabsf(float x);
+long double fabsl(long double x);
+#endif /* _ABS_FLOAT_DEFINED */
+
+static inline double      fdim(double x, double y) { return __builtin_fdim(x, y); }
+static inline float       fdimf(float x, float y) { return __builtin_fdimf(x, y); }
+static inline long double fdiml(long double x, long double y) { return __builtin_fdiml(x, y); }
+
+static inline double      floor(double x) { return __builtin_floor(x); }
+static inline float       floorf(float x) { return __builtin_floorf(x); }
+static inline long double floorl(long double x) { return __builtin_floorl(x); }
+
+static inline double      fma(double x, double y, double z) { return __builtin_fma(x, y, z); }
+static inline float       fmaf(float x, float y, float z) { return __builtin_fmaf(x, y, z); }
+static inline long double fmal(long double x, long double y, long double z) { return __builtin_fmal(x, y, z); }
+
+static inline double      fmax(double x, double y) { return __builtin_fmax(x, y); }
+static inline float       fmaxf(float x, float y) { return __builtin_fmaxf(x, y); }
+static inline long double fmaxl(long double x, long double y) { return __builtin_fmaxl(x, y); }
+
+static inline double      fmin(double x, double y) { return __builtin_fmin(x, y); }
+static inline float       fminf(float x, float y) { return __builtin_fminf(x, y); }
+static inline long double fminl(long double x, long double y) { return __builtin_fminl(x, y); }
+
+static inline double      fmod(double x, double y) { return __builtin_fmod(x, y); }
+static inline float       fmodf(float x, float y) { return __builtin_fmodf(x, y); }
+static inline long double fmodl(long double x, long double y) { return __builtin_fmodl(x, y); }
+
+static inline double      frexp(double x, int *e)  { return __builtin_frexp(x, e); }
+static inline float       frexpf(float x, int *e)  { return __builtin_frexpf(x, e); }
+static inline long double frexpl(long double x, int *e)  { return __builtin_frexpl(x, e); }
+
+static inline double      hypot(double x, double y) { return __builtin_hypot(x, y); }
+static inline float       hypotf(float x, float y) { return __builtin_hypotf(x, y); }
+static inline long double hypotl(long double x, long double y) { return __builtin_hypotl(x, y); }
+
+double      __hypot3(double, double, double);
+float       __hypot3f(float, float, float);
+long double __hypot3l(long double, long double, long double);
+
+static inline int         ilogb(double x) { return __builtin_ilogb(x); }
+static inline int         ilogbf(float x) { return __builtin_ilogbf(x); }
+static inline int         ilogbl(long double x) { return __builtin_ilogbl(x); }
+
+static inline double      ldexp(double x, int e) { return __builtin_ldexp(x, e); }
+static inline float       ldexpf(float x, int e) { return __builtin_ldexpf(x, e); }
+static inline long double ldexpl(long double x, int e) { return __builtin_ldexpl(x, e); }
+
+static inline double      lgamma(double x) { return __builtin_lgamma(x); }
+static inline float       lgammaf(float x) { return __builtin_lgammaf(x); }
+static inline long double lgammal(long double x) { return __builtin_lgammal(x); }
+
+static inline long long   llrint(double x) { return __builtin_llrint(x); }
+static inline long long   llrintf(float x) { return __builtin_llrintf(x); }
+static inline long long   llrintl(long double x) { return __builtin_llrintl(x); }
+
+static inline long long   llround(double x) { return __builtin_llround(x); }
+static inline long long   llroundf(float x) { return __builtin_llroundf(x); }
+static inline long long   llroundl(long double x) { return __builtin_llroundl(x); }
+
+static inline double      log(double x) { return __builtin_log(x); }
+static inline float       logf(float x) { return __builtin_logf(x); }
+static inline long double logl(long double x) { return __builtin_logl(x); }
+
+static inline double      log10(double x) { return __builtin_log10(x); }
+static inline float       log10f(float x) { return __builtin_log10f(x); }
+static inline long double log10l(long double x) { return __builtin_log10l(x); }
+
+static inline double      log1p(double x) { return __builtin_log1p(x); }
+static inline float       log1pf(float x) { return __builtin_log1pf(x); }
+static inline long double log1pl(long double x) { return __builtin_log1pl(x); }
+
+static inline double      log2(double x) { return __builtin_log2(x); }
+static inline float       log2f(float x) { return __builtin_log2f(x); }
+static inline long double log2l(long double x) { return __builtin_log2l(x); }
+
+static inline double      logb(double x) { return __builtin_logb(x); }
+static inline float       logbf(float x) { return __builtin_logbf(x); }
+static inline long double logbl(long double x) { return __builtin_logbl(x); }
+
+static inline long        lrint(double x) { return __builtin_lrint(x); }
+static inline long        lrintf(float x) { return __builtin_lrintf(x); }
+static inline long        lrintl(long double x) { return __builtin_lrintl(x); }
+
+static inline long        lround(double x) { return __builtin_lround(x); }
+static inline long        lroundf(float x) { return __builtin_lroundf(x); }
+static inline long        lroundl(long double x) { return __builtin_lroundl(x); }
+
+static inline double      modf(double x, double *ip)  { return __builtin_modf(x, ip); }
+static inline float       modff(float x, float *ip)  { return __builtin_modff(x, ip); }
+static inline long double modfl(long double x, long double *ip)  { return __builtin_modfl(x, ip); }
+
+static inline double      nan(const char *s) { return __builtin_nan(s); }
+static inline float       nanf(const char *s) { return __builtin_nanf(s); }
+static inline long double nanl(const char *s) { return __builtin_nanl(s); }
+
+static inline double      nearbyint(double x) { return __builtin_nearbyint(x); }
+static inline float       nearbyintf(float x) { return __builtin_nearbyintf(x); }
+static inline long double nearbyintl(long double x) { return __builtin_nearbyintl(x); }
+
+static inline double      nextafter(double x, double y) { return __builtin_nextafter(x, y); }
+static inline float       nextafterf(float x, float y) { return __builtin_nextafterf(x, y); }
+static inline long double nextafterl(long double x, long double y) { return __builtin_nextafterl(x, y); }
+
+double      nextdown(double);
+float       nextdownf(float);
+long double nextdownl(long double);
+
+static inline double      nexttoward(double x, long double y) { return __builtin_nexttoward(x, y); }
+static inline float       nexttowardf(float x, long double y) { return __builtin_nexttowardf(x, y); }
+static inline long double nexttowardl(long double x, long double y) { return __builtin_nexttowardl(x, y); }
+
+double      nextup(double);
+float       nextupf(float);
+long double nextupl(long double);
+
+static inline double      pow(double x, double y) { return __builtin_pow(x, y); }
+static inline float       powf(float x, float y) { return __builtin_powf(x, y); }
+static inline long double powl(long double x, long double y) { return __builtin_powl(x, y); }
+
+static inline double      remainder(double x, double y) { return __builtin_remainder(x, y); }
+static inline float       remainderf(float x, float y) { return __builtin_remainderf(x, y); }
+static inline long double remainderl(long double x, long double y) { return __builtin_remainderl(x, y); }
+
+static inline double      remquo(double x, double y, int *q)  { return __builtin_remquo(x, y, q); }
+static inline float       remquof(float x, float y, int *q)  { return __builtin_remquof(x, y, q); }
+static inline long double remquol(long double x, long double y, int *q)  { return __builtin_remquol(x, y, q); }
+
+static inline double      rint(double x) { return __builtin_rint(x); }
+static inline float       rintf(float x) { return __builtin_rintf(x); }
+static inline long double rintl(long double x) { return __builtin_rintl(x); }
+
+static inline double      round(double x) { return __builtin_round(x); }
+static inline float       roundf(float x) { return __builtin_roundf(x); }
+static inline long double roundl(long double x) { return __builtin_roundl(x); }
+
+double      roundeven(double);
+float       roundevenf(float);
+long double roundevenl(long double);
+
+static inline double      scalbln(double x, long n) { return __builtin_scalbln(x, n); }
+static inline float       scalblnf(float x, long n) { return __builtin_scalblnf(x, n); }
+static inline long double scalblnl(long double x, long n) { return __builtin_scalblnl(x, n); }
+
+static inline double      scalbn(double x, int n) { return __builtin_scalbn(x, n); }
+static inline float       scalbnf(float x, int n) { return __builtin_scalbnf(x, n); }
+static inline long double scalbnl(long double x, int n) { return __builtin_scalbnl(x, n); }
+
+static inline double      sin(double x) { return __builtin_sin(x); }
+static inline float       sinf(float x) { return __builtin_sinf(x); }
+static inline long double sinl(long double x) { return __builtin_sinl(x); }
+
+double      sinpi(double);
+float       sinpif(float);
+long double sinpil(long double);
+
+static inline double      sinh(double x) { return __builtin_sinh(x); }
+static inline float       sinhf(float x) { return __builtin_sinhf(x); }
+static inline long double sinhl(long double x) { return __builtin_sinhl(x); }
+
+static inline double      sqrt(double x) { return __builtin_sqrt(x); }
+static inline float       sqrtf(float x) { return __builtin_sqrtf(x); }
+static inline long double sqrtl(long double x) { return __builtin_sqrtl(x); }
+
+static inline double      tan(double x) { return __builtin_tan(x); }
+static inline float       tanf(float x) { return __builtin_tanf(x); }
+static inline long double tanl(long double x) { return __builtin_tanl(x); }
+
+double      tanpi(double);
+float       tanpif(float);
+long double tanpil(long double);
+
+static inline double      tanh(double x) { return __builtin_tanh(x); }
+static inline float       tanhf(float x) { return __builtin_tanhf(x); }
+static inline long double tanhl(long double x) { return __builtin_tanhl(x); }
+
+static inline double      tgamma(double x) { return __builtin_tgamma(x); }
+static inline float       tgammaf(float x) { return __builtin_tgammaf(x); }
+static inline long double tgammal(long double x) { return __builtin_tgammal(x); }
+
+static inline double      trunc(double x) { return __builtin_trunc(x); }
+static inline float       truncf(float x) { return __builtin_truncf(x); }
+static inline long double truncl(long double x) { return __builtin_truncl(x); }
+
+#endif
 
 #ifdef __cplusplus
 }
