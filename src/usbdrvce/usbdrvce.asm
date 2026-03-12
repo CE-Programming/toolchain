f53e1cfabd90507cccfcd9c24092fe3e3d47f4b3
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-03-12 16:46:20 -0600

    optimize validInterval branch

--- src/usbdrvce/usbdrvce.asm
+++ src/usbdrvce/usbdrvce.asm
@@ -2952,8 +2952,8 @@ assert endpointDescriptor.wMaxPacketSize+2 = endpointDescriptor.bInterval
 	inc	hl
 	ld	b,(hl)
 	inc	b
-	djnz	.validInterval
-	jq	.invalidParam
+	dec	b
+	jq	z, .invalidParam
 .validInterval:
 	ex	de,hl
 	inc.s	hl
