fe089392e2a46399721619208a708e0b82bcb75c
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-03-29 19:47:24 -0600

    added tests for fcmp/dcmp

--- test/floating_point/compare/src/cmp_test.s
+++ test/floating_point/compare/src/cmp_test.s
@@ -2,6 +2,22 @@
 
 	.section	.text
 
+	.global	_crt_fcmp
+	.type	_crt_fcmp, @function
+_crt_fcmp:
+	ld	iy, 0
+	add	iy, sp
+	ld	hl, (iy + 3)
+	ld	e, (iy + 6)
+	ld	bc, (iy + 9)
+	ld	a, (iy + 12)
+	call	__fpcmp
+	push	af
+	pop	hl
+	ld	a, l
+	ret
+	.extern	__fpcmp
+
 	.global	_crt_fcmpo
 	.type	_crt_fcmpo, @function
 _crt_fcmpo:
@@ -33,6 +49,28 @@ _crt_fcmpu:
 	ld	a, l
 	ret
 	.extern	__fpcmpu
+
+	.global	_crt_dcmp
+	.type	_crt_dcmp, @function
+_crt_dcmp:
+	ld	iy, 0
+	add	iy, sp
+	ld	hl, (iy + 18)
+	push	hl
+	ld	hl, (iy + 15)
+	push	hl
+	ld	hl, (iy + 12)
+	push	hl
+	ld	bc, (iy + 9)
+	ld	de, (iy + 6)
+	ld	hl, (iy + 3)
+	call	__dcmp
+	push	af
+	pop	hl
+	ld	a, l
+	ld	sp, iy
+	ret
+	.extern	__dcmp
 
 	.global	_crt_dcmpo
 	.type	_crt_dcmpo, @function
