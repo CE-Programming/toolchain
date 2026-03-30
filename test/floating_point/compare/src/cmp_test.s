4a23c615c659b45c1066500ccb447dcdeafa05c9
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-03-29 19:07:20 -0600

    added dcmp(o/u) and tests for (f/d)cmp(o/u)

---
+++ test/floating_point/compare/src/cmp_test.s
@@ -0,0 +1,79 @@
+	.assume	adl=1
+
+	.section	.text
+
+	.global	_crt_fcmpo
+	.type	_crt_fcmpo, @function
+_crt_fcmpo:
+	ld	iy, 0
+	add	iy, sp
+	ld	hl, (iy + 3)
+	ld	e, (iy + 6)
+	ld	bc, (iy + 9)
+	ld	a, (iy + 12)
+	call	__fpcmpo
+	push	af
+	pop	hl
+	ld	a, l
+	ret
+	.extern	__fpcmpo
+
+	.global	_crt_fcmpu
+	.type	_crt_fcmpu, @function
+_crt_fcmpu:
+	ld	iy, 0
+	add	iy, sp
+	ld	hl, (iy + 3)
+	ld	e, (iy + 6)
+	ld	bc, (iy + 9)
+	ld	a, (iy + 12)
+	call	__fpcmpu
+	push	af
+	pop	hl
+	ld	a, l
+	ret
+	.extern	__fpcmpu
+
+	.global	_crt_dcmpo
+	.type	_crt_dcmpo, @function
+_crt_dcmpo:
+	ld	iy, 0
+	add	iy, sp
+	ld	hl, (iy + 18)
+	push	hl
+	ld	hl, (iy + 15)
+	push	hl
+	ld	hl, (iy + 12)
+	push	hl
+	ld	bc, (iy + 9)
+	ld	de, (iy + 6)
+	ld	hl, (iy + 3)
+	call	__dcmpo
+	push	af
+	pop	hl
+	ld	a, l
+	ld	sp, iy
+	ret
+	.extern	__dcmpo
+
+	.global	_crt_dcmpu
+	.type	_crt_dcmpu, @function
+_crt_dcmpu:
+	ld	iy, 0
+	add	iy, sp
+	ld	hl, (iy + 18)
+	push	hl
+	ld	hl, (iy + 15)
+	push	hl
+	ld	hl, (iy + 12)
+	push	hl
+	ld	bc, (iy + 9)
+	ld	de, (iy + 6)
+	ld	hl, (iy + 3)
+	call	__dcmpu
+	push	af
+	pop	hl
+	ld	a, l
+	ld	sp, iy
+	ret
+	.extern	__dcmpu
