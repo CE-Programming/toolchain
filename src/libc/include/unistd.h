db3f752494b17f5c2a590dcfe3c1c243ee7cfdc2
zerico <71151164+ZERICO2005@users.noreply.github.com>
2026-04-22 10:26:02 -0600

    add sleep/usleep/useconds_t to unistd.h

--- src/libc/include/unistd.h
+++ src/libc/include/unistd.h
@@ -3,6 +3,13 @@
 
 #include <cdefs.h>
 
+/*
+ * unsigned int sleep(unsigned int seconds);
+ * typedef unsigned int useconds_t;
+ * int usleep(useconds_t usec);
+ */
+#include <sys/timers.h>
+
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
